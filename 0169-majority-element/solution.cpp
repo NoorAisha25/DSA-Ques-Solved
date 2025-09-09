4ecc20c088f59bb0e7f9b4a2d8aeccfece373a56
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2025-09-09 16:51:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.4 MB (12.29%)

---
+++ 0169-majority-element/solution.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+        /*for(int i=0; i<n ; i++){
+            int cnt=0;
+            for(int j=0; j<n; j++){
+                if(nums[i]==nums[j]){
+                    cnt++;
+                }
+            } if(cnt>n/2){ return nums[i];}
+        }*/
+        map<int,int>mpp;
+        for(int i=0; i<n; i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it : mpp){
+            if(it.second > n/2 ){
+                return it.first ; 
+            }
+        }
+    return -1; } 
+};
