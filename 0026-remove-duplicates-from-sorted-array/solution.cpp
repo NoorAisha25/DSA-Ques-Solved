a8b31409a0a39a19030157822b7a192ceda56bb2
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2025-09-14 08:45:19 +0000

    Sync LeetCode submission Runtime - 1 ms (15.09%), Memory - 22.6 MB (52.01%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -12,12 +12,13 @@ public:
         }Not inplace approach so do another two pointer apprach
         */
 
+        // 
         int i=0;
-        for(int j=1;j<nums.size();j++){
-            if(nums[i]!=nums[j]){
-                nums[i+1]=nums[j];
+        for(int j=1; j<nums.size() ; j++){
+            if(nums[i] != nums[j]){
+                nums[i+1] = nums[j];
                 i++;
             }
-        }return (i+1);
+        } return (i+1);
     }
 };
