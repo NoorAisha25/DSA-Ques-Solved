85ec63009add8245b61d1ac3b3a50bbba3c188b2
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2025-09-14 15:19:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.7 MB (70.79%)

--- 0268-missing-number/solution.cpp
+++ 0268-missing-number/solution.cpp
@@ -14,12 +14,29 @@ public:
         } return -1;
     }
     */
-    int n=nums.size();
-    int sum=(n*(n+1))/2;
-    int s2=0;
-    for(int i=0 ; i<n ; i++){
-             s2+= nums[i];
-    }
-    return (sum-s2);
-    }
+     
+     int n=nums.size();
+    // int sum=(n*(n+1))/2;
+    // int s2=0;
+    // for(int i=0 ; i<n ; i++){
+    //          s2+= nums[i];
+    // }
+    // return (sum-s2);
+//practice
+    // vector<int> hash(n+1,0);
+    // for(int i=0 ;i<n ; i++){
+    //     hash[nums[i]]=1;
+    // }
+    // for(int i=0 ;i<n+1 ; i++){
+    //     if(hash[i]==0){
+    //         return i;
+    //     }
+    // }
+    //  
+     int sum = n*(n+1)/2;
+     int s2 = 0;
+     for(int i=0; i<n; i++){
+        s2 += nums[i];
+     } return (sum-s2);
+    return -1;}
 };
