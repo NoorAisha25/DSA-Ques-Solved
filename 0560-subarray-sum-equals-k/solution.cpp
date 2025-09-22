e6c4f1fbd928bf1b2a0db1ab0f645ed86be64264
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2025-09-22 19:26:13 +0000

    Sync LeetCode submission Runtime - 50 ms (42.06%), Memory - 51 MB (18.14%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        //Better approach with TC : O (n^2)
-    //     int count=0;
-    //     for(int i=0 ; i<nums.size() ; i++){
-    //         int sum=0;
-            
-    //         for(int j=i ; j<nums.size(); j++){
-    //             sum+=nums[j];
-    //             if(sum==k) count++;
-    //         }
-            
-    //     }return count;
-    // return -1;
-    int n= nums.size();
-    unordered_map<int,int> mpp;
-    mpp[0] = 1;
-    int presum= 0 ; int cnt=0;
-    for(int i= 0 ; i<n ; i++){
-        presum += nums[i];
-        int remove = presum - k;
-        cnt += mpp[remove];
-        mpp [presum] +=1;
-    }
-    return cnt;
+        int n=nums.size();
+        
+       int prefixSum= 0 , cnt=0 ;
+       unordered_map<int,int> mpp;
+       mpp[0] = 1;  // insures sunarray counts from beginning if found
+       for(int i=0 ; i<n ; i++){
+             prefixSum += nums[i]; 
+             int remove = prefixSum - k;
+             cnt += mpp[remove];
+             mpp[prefixSum] += 1;
+       }
+
+          return cnt; 
     }
 };
+
+
