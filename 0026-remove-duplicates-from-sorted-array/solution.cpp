9e6bf3a27ecbe4d907620cf26509718dea7570ca
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2025-10-28 06:04:17 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.6 MB (80.31%)

--- 0026-remove-duplicates-from-sorted-array/solution.cpp
+++ 0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-       /* set<int>st;
-        for(int i=0; i<nums.size(); i++){
-            st.insert(nums[i]);
-        }
-        int index=0;
-        for(auto it:st){
-            nums[index]=it;
-            index++;
-        }Not inplace approach so do another two pointer apprach
-        */
-
-        // 
+        int n = nums.size();
         int i=0;
-        for(int j=1; j<nums.size() ; j++){
-            if(nums[i] != nums[j]){
-                nums[i+1] = nums[j];
-                i++;
-            }
-        } return (i+1);
-    }
+            for(int j = 1 ; j<n ; j++){
+                if(nums[i] != nums[j]){
+                   nums[i+1] = nums[j];
+                    i++;
+                }
+            } return (i+1);
+        }  
 };
