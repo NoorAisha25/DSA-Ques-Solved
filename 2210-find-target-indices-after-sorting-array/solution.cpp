0f6a095a7d3e25d1e27033100b318bb935f3399f
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-06 13:46:10 +0000

    Sync LeetCode submission Runtime - 7 ms (0.76%), Memory - 15.3 MB (16.09%)

--- 2210-find-target-indices-after-sorting-array/solution.cpp
+++ 2210-find-target-indices-after-sorting-array/solution.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-        int n=nums.size();
-        sort(nums.begin(), nums.end());
-        vector<int>ans;
+        
+        int n = nums.size();
+        for(int i=0 ; i<n ; i++){
+            sort(nums.begin(), nums.end());
+        }
+        vector<int>ans ; 
         for(int i=0 ; i<n ; i++){
             if(nums[i]==target){
                 ans.push_back(i);
             }
-        } return ans;
+        }
+        return ans;
     }
 };
