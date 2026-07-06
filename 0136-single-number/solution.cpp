1b921905fa049ecb4c2e39ab9aefa021dc1b94f6
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-06 16:51:43 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.6 MB (58.52%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         int n = nums.size();
-        int xorr = 0;
+        int x=0;
         for(int i=0 ; i<n ; i++){
-            xorr = xorr ^ nums[i];
-        } return xorr;
-        
-         }
+            x ^= nums[i];
+        }
+        return x;
+    }
 };
