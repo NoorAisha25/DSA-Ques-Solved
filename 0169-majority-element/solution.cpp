8c40a90e3bcb33602cd42b9e9b3581be2872db10
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-07 16:25:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 28.1 MB (95.45%)

--- 0169-majority-element/solution.cpp
+++ 0169-majority-element/solution.cpp
@@ -2,27 +2,40 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
-        /*for(int i=0; i<n ; i++){
-            int cnt=0;
-            for(int j=0; j<n; j++){
-                if(nums[i]==nums[j]){
-                    cnt++;
-                }
-            } if(cnt>n/2){ return nums[i];}
-        }*/
-    //     
-    
-    map<int,int> mpp;
-    for(int i=0 ; i<n ; i++){
-        mpp[nums[i]]++;
-    }
-    for(auto it : mpp){
-        if(it.second > n/2){
-            return it.first;
+        /* --- Brute force ---*/
+        // for(int i=0 ; i<n ; i++){
+        //     int cnt=0;
+        //     for(int j=i ; j<n ; j++){
+        //         if(nums[i]==nums[j]) cnt++;
+        //     }
+        //     if(cnt>(n/2)) return nums[i];
+        // }
+        // return -1;
+
+        /*----hashmap-----*/
+    //     map<int,int>mp;
+    //     for(int i=0 ; i<n ; i++){
+    //         mp[nums[i]]++;
+    //     }
+    //     for(auto it: mp){
+    //         if(it.second>n/2) return it.first;
+    //     }
+
+    //    return -1;
+        /*--moore's voting algorithm --*/
+        int count=0;
+        int majEle = nums[0];
+        for(int i=0 ; i<n ; i++){
+            if(count==0){
+                majEle= nums[i];
+                count=1;
+            }
+            else if(nums[i]==majEle){
+                count++;
+            }
+            else count--;
         }
-    }
-    
-     return -1;
-     
+        return majEle;
+       
      } 
 };
