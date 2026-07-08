3849d5679eef430ab29aed36424d681e93347c74
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-08 06:39:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.7 MB (11.31%)

--- 0075-sort-colors/solution.cpp
+++ 0075-sort-colors/solution.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // int n=nums.size();
-        // int cnt0=0 , cnt1=0 , cnt2=0;
-        // for(int i=0; i<n ; i++){
-        //     if(nums[i]==0) cnt0++;
-        //     else if(nums[i]==1) cnt1++;
-        //     else(nums[i]==2) cnt2++;
-        //  }
-        //  for(int i=0 ; i<cnt0 ; i++) nums[i]=0;
-        //  for(int i=cnt0 ; i<cnt0+cnt1 ; i++) nums[i]=1;
-        //  for(int i=cnt0+cnt1 ; i<n ; i++) nums[i]=2;
-        int n = nums.size();
-        int mid = 0 ;
-        int low = 0;
-        int high = n-1;
-        while(mid <= high){
-            if(nums[mid]==0){
-                 swap(nums[low] , nums[mid]);
-                 low++;
-                 mid++;
-            }
-            else if(nums[mid]==1){
-                    mid++;
-            }
-            else{
-                    swap(nums[mid] , nums[high]);
-                    high--;
-            }
-        }
+        int n=nums.size();
+        /*---Dutch National Flag Algorithm ---*/
+        int i = 0 ; // denotes 0
+        int j = 0; //denotes 1
+        int k = n-1; //denotes 2
+
+        while(j<=k){
+                if(nums[j] == 1){
+                    j++;
+                }
+                else if(nums[j] == 2){
+                    swap(nums[j], nums[k]);
+                    k--;
+                }
+                else{
+                    // when nums[j]==0
+                    swap(nums[j], nums[i]);
+                    i++;
+                    j++;
+                }
+        } 
     }
 };
