35a1d89cad9fbe02015fa9ce4af698ed9d4dfc2d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-08 07:28:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 71.8 MB (14.79%)

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // int n = nums.size();
-        // int maxi = INT_MIN;
-        // for(int i=0 ; i<n ; i++){
-        //     int sum=0;
-        //     for(int j=i ; j<n ; j++){
-        //         sum+= nums[j];
-        //         maxi = max(sum, maxi);
-        //     }
-        // }return maxi;
-        int n=nums.size() ;
-        int sum= 0;
-        int maxi= INT_MIN;
-        for(int i=0; i<n; i++){
-            sum+= nums[i];
-            if(sum>maxi){
-                maxi = sum;
+        int n = nums.size();
+        /*---bY THIS BRUTE FORCE APPROACH of TC-O(n^2) ony 204/210 test cases pass----
+        int maxi = INT_MIN;
+        
+        for(int i=0 ; i<n ; i++){
+            int sum=0;
+            for(int j=i ; j<n ; j++){
+                sum += nums[j];
+                maxi = max(maxi, sum);
             }
+        }
+        return maxi;
+        -------------------------------------*/
 
-            if(sum<0){
-                sum=0;
-            }
-        } return maxi;
+        // Kadane's Algorithms ---DynamicProgramming---TC=O(n^2)
+        int CurrSum = 0 ;
+        int MaxiSum = INT_MIN ;
+        for(int i=0 ; i<n ; i++){
+            CurrSum += nums[i];
+            MaxiSum = max(MaxiSum , CurrSum);
+            if(CurrSum <0) CurrSum=0;
+        }
+        return MaxiSum;
     }
 };
