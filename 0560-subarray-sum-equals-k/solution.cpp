7faeb87a7dc2f457786bd6aead260d17d26def37
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-10 07:47:52 +0000

    Sync LeetCode submission Runtime - 35 ms (85.60%), Memory - 45.5 MB (55.08%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -1,16 +1,35 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int cnt=0; // sum not taken here bcz if we iinc i then sum should be equal to 0
+        int n = nums.size();
+        // int cnt=0;
+         
+        // for(int i=0 ; i<n ; i++){
+        //     int sum = 0;
+        //     for(int j= i ; j<n ; j++){
+        //         sum += nums[j];
+
+        //         if(sum == k) cnt++;
+        //     }
+        // }
+        // return cnt;
+
+        unordered_map<int,int> mpp;
+        
+        int result = 0;
+        int cumSum = 0;
+        mpp.insert({0,1});
+
         for(int i=0 ; i<n ; i++){
-            int sum = 0;
-            for(int j=i ; j<n ; j++){
-                sum += nums[j];
-                if(sum==k) cnt++;
+            cumSum += nums[i];
+            //when found cumSum-k in the map so increase result be 1
+            if(mpp.find(cumSum-k) != mpp.end()){
+                result += mpp[cumSum-k];
             }
+            mpp[cumSum]++;
         }
-        return cnt;
+        return result;
+
     }
 };
 
