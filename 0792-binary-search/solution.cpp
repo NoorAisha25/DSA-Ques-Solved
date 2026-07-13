effee9f6d8d56eb5bb0a624208e558f7ada0d637
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-13 11:27:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 31.3 MB (45.76%)

--- 0792-binary-search/solution.cpp
+++ 0792-binary-search/solution.cpp
@@ -2,21 +2,29 @@ class Solution {
 public:
     
     int search(vector<int>& nums , int target){
-        int n = nums.size();
-        int l = 0 , h = n - 1;
-        while( l<= h){
-            int m = (l + h)/2;
-            if(nums[m] == target) return m;
-            if(nums[l] <= nums[m]){
-                if(nums[l] <= target && target <= nums[m])
-                h = m - 1;
-            else l = m + 1;
-            }
-            else{
-                if(nums[m] <= target && target <= nums[h])
-                l = m + 1;
-            else h = m - 1;
-            }
-        } return -1;
+         int n = nums.size();
+         
+         int l = 0;
+         int h = n-1;
+
+         while( l<= h ){
+
+            int mid = (l + h) / 2;
+
+            if(nums[mid] == target) return mid;
+
+            else if(nums[mid] > target) h = mid - 1;
+
+            else l = mid + 1;
+         }
+         return -1;
+/*------This is linear search approach where tc-O(n) ------*/
+        // int n = nums.size();
+
+        // for(int i=0 ; i<n ; i++){
+        //     if(nums[i] == target) return i;
+            
+        // }
+        // return -1;
     }
 };
