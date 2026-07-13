3b462e9304083ec0ed93bfd6e21491722624271d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-13 12:13:54 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (43.57%)

--- 0035-search-insert-position/solution.cpp
+++ 0035-search-insert-position/solution.cpp
@@ -2,15 +2,35 @@ class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
         int n= nums.size();
-        int low = 0 ; int high = n-1;
-        int ans = n;
-        while(low<= high){
-            int mid = low + (high -low)/2;
-            if(nums[mid] >= target){
-                ans = mid;
-                high = mid-1;
+        // int low = 0 ; int high = n-1;
+        // int ans = n;
+        // while(low<= high){
+        //     int mid = low + (high -low)/2;
+        //     if(nums[mid] >= target){
+        //         ans = mid;
+        //         high = mid-1;
+        //     }
+        //     else low = mid+1;
+        // } return ans;
+         
+        /* ---- this question is similar to that o flower bound problem ---*/
+
+        int l = 0 , h = n - 1 , ans = n;
+         
+         while( l <= h){
+            
+            int mid = ( l + h ) / 2;
+
+            if ( nums [ mid ] >= target){
+
+                ans = mid ;
+                h= mid -1;
             }
-            else low = mid+1;
-        } return ans;
+
+            else l = mid + 1;
+         }
+         return ans ;
+
+
     }
 };
