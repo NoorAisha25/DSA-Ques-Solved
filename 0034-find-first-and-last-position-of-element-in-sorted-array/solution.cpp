d2daa1c370e69e272016443a5627051a0c840d47
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-13 13:44:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.5 MB (53.25%)

--- 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -1,35 +1,51 @@
 class Solution {
 public:
-    int lowerb(vector<int>& nums, int target){
+
+    int lowerB( vector<int> &nums , int target){
         int n = nums.size();
-        int l = 0 , h = n -1;
-        int ans = n ;
+        int l = 0 ;
+        int h = n - 1;
+        int ans = n;
+
         while( l <= h){
-            int m = (l + h)/2;
-            if(nums[m] >= target){
-                ans = m;
-                h = m - 1;
+
+            int mid = (l + h)/2;
+
+            if( nums[mid] >= target){
+
+                ans = mid ;
+                h = mid - 1;
             }
-            else l = m + 1;
-        } return ans;
+            else l = mid + 1;
+        }
+
+        return ans;
     }
-    int upperb(vector<int>& nums , int target){
+    
+    int upperB( vector<int> &nums , int target){
         int n = nums.size();
-        int l = 0 , h = n -1;
-        int ans = n ;
+        int l = 0 ;
+        int h = n - 1;
+        int ans = n;
+
         while( l <= h){
-            int m = (l + h)/2;
-            if(nums[m] > target){
-                ans = m;
-                h = m - 1;
+
+            int mid = (l + h)/2;
+
+            if( nums[mid] > target){
+
+                ans = mid ;
+                h = mid - 1;
             }
-            else l = m + 1;
-        } return ans;
+            else l = mid + 1;
+        }
+
+        return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
-       int lb = lowerb(nums , target);
-       if( lb == nums.size() || nums[lb] !=target) return {-1 ,-1};
-       int ub = upperb(nums , target);
-       return {lb , ub-1};
+       int lb = lowerB( nums , target );
+       if( lb == nums.size() || nums[lb] != target) return {-1 , -1};
+       int ub = upperB( nums, target );
+       return {lb , ub-1 };
     }
 };
