909e214deee3b6f4c7f35b2f6d49412d1bbb8684
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-14 15:15:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.6 MB (83.26%)

---
+++ 0162-find-peak-element/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int l = 0 ;
+        int r= n-1;
+        while( l< r){
+            int m = (l+r)/2;
+            if(nums[m] < nums[m+1]) l = m+1;
+            else r = m;
+        } 
+        return l;
+    }
+};
