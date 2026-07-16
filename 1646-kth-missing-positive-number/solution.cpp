69034f34587ecc9a7b851da6106c08dbb83678c5
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-16 11:00:29 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (60.90%)

---
+++ 1646-kth-missing-positive-number/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        // int num = 1;
+        // int i=0;
+        // int n = arr.size();
+
+        // while(i<n && k>0){
+        //     if(arr[i] == num){
+        //           i++;
+        //     }
+        //     else{
+        //         k--;
+        //     }
+        //     num++;
+        // }  
+        // while(k--)    {
+        //     num++;
+        // }
+        // return num-1;
+        int n = arr.size();
+        int l = 0 ;
+        int h = n-1;
+
+        while( l<=h ){
+           
+           int m = l + (h-l)/2;
+
+           int MissingNum = arr[m] - (m + 1);
+
+           if(MissingNum < k) l = m + 1;
+           else h = m - 1;
+        }
+        return l+k;
+    }
+};
