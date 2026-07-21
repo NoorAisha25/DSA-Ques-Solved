7cf7b2e5db489a18f232011c6bb6991f461800e0
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-21 16:05:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (90.45%)

---
+++ 0908-middle-of-the-linked-list/solution.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // ListNode* temp = head;
+        // int cnt=0;
+        // while(temp != NULL){
+        //     cnt++;
+        //     temp = temp-> next;
+        // }
+        // int middleNode = (cnt / 2)+1;
+        // temp = head;
+        // while(temp != NULL){
+        //    middleNode = middleNode - 1;
+        //    if(middleNode == 0) break;
+        //    temp = temp->next;
+        // }
+        // return temp;
+/*-----------TortoiseHare Solution Approach-----------------*/
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+};
