2ffbecd947f4025f7912ed7bbc77f67ed6fc679f
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-25 08:45:11 +0000

    Sync LeetCode submission Runtime - 3 ms (67.68%), Memory - 14.9 MB (28.97%)

--- 0001-two-sum/solution.cpp
+++ 0001-two-sum/solution.cpp
@@ -2,14 +2,25 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n=nums.size();
-        map<int,int>mp;
-        for(int i=0 ; i<n ; i++){
-            int a= nums[i];
-            int more= target-a;
-            if(mp.find(more) != mp.end()){
-                return {mp[more],i};
+        // map<int,int>mp;
+        // for(int i=0 ; i<n ; i++){
+        //     int a= nums[i];
+        //     int more= target-a;
+        //     if(mp.find(more) != mp.end()){
+        //         return {mp[more],i};
+        //     }
+        //     mp[a]=i;
+        // }
+        // return {};
+        map<int,int>mpp;
+
+        for(int i = 0 ; i< n ; i++){
+            int a = nums[i];
+            int more = target - a;
+            if(mpp.find(more) != mpp.end()){
+                return {mpp[more] , i};
             }
-            mp[a]=i;
+            mpp[a] = i;
         }
         return {};
     } 
