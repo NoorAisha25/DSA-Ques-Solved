dbc4861e6153468ff790c700de1eb8b41b3414e1
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-26 11:18:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 26.2 MB (72.06%)

--- 0540-single-element-in-a-sorted-array/solution.cpp
+++ 0540-single-element-in-a-sorted-array/solution.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int n = nums.size();
+        int l = 0;
+        int h = nums.size()-1;
         
-        int l = 0 ;
-        int h = n - 1;
+        if(nums.size() == 1) return nums[0];
+        if(nums[l] != nums[l+1]) return nums[l];
+        if(nums[h] != nums[h-1]) return nums[h];
 
-        if(n==1) return nums[0];
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-1]) return nums[n-1];
+        while(l <= h){
 
-        while( l <= h){
+            int mid = l + ( h - l )/2;
 
-            int m = (l+h)/2;
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1]) return nums[mid];
 
-            if( nums[m] != nums[m-1] && nums[m] != nums[m+1]) return nums[m];
-         //eliminating right half whether even or odd
-            if( ( m%2 == 1  && nums[m] == nums[m-1]) || (m%2 == 0  && nums[m] == nums[m+1])) l = m+1;
-            // eliminating left half
-            else h = m - 1;
+            else if( (mid % 2 == 0 && nums[mid] == nums[mid-1]) || (mid % 2 == 1 && nums[mid] == nums[mid+1])){
+                h = mid - 1;
+            }
+            else l = mid + 1;
         }
         return -1;
     } 
