a9d42bec3ea59a61334576e32c7f6f90211d298c
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-28 10:10:53 +0000

    Sync LeetCode submission Runtime - 2 ms (38.15%), Memory - 8.4 MB (83.13%)

--- 0007-reverse-integer/solution.cpp
+++ 0007-reverse-integer/solution.cpp
@@ -3,12 +3,12 @@ public:
     int reverse(int x) {
         long long rev = 0;
         int temp = x;
-        while (temp != 0){
+        while(temp != 0){
             int d = temp%10;
             temp = temp/10;
-            rev = (rev * 10)+ d;
+            rev = ( rev * 10)+ d;
         }
-        if(rev < INT_MIN || rev > INT_MAX) return 0;
-        return(rev);
+        if(rev <INT_MIN || rev > INT_MAX) return 0;
+        return rev;
     }
 };
