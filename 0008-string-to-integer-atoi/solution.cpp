9c86fdcc827db8bd9c2ead2d5e9b2493ee87e764
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-30 15:19:25 +0000

    Sync LeetCode submission Runtime - 3 ms (12.86%), Memory - 10.4 MB (5.17%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
+    int helper(string s , int i , long long res , int sign){
+           if(i >= s.size() || !isdigit(s[i])){
+                return (int)(sign * res);
+           }
+
+            int digit = s[i] - '0';
+
+            if(res > INT_MAX/10 || (res == INT_MAX/10 && digit > 7)){
+                return (sign == 1) ? INT_MAX : INT_MIN ;
+            }
+
+            res = res * 10 + digit;
+            i++;
+        
+        return helper(s , i , res , sign);
+    }
     int myAtoi(string s) {
-        int i = 0;
+        long res = 0;
         int n = s.length();
-        long res = 0 ;
+        int i = 0;
         int sign = 1;
 
-        //handle whitespaces 
-        while(i<n && s[i] == ' ') i++;
+        // skipping the white space
+        while(i < n && s[i] ==  ' ' ){ i++;}
 
-        //handle sign
-        if(i<n && (s[i] == '+' || s[i] == '-')){
+        if(i < n && (s[i] == '-' || s[i] == '+')){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
 
-        //handle digits
-        while(i<n && isdigit(s[i])){
-            int digit = s[i] - '0';
-
-            //check overflow
-            if(res > INT_MAX/10 || (res == INT_MAX/10 && digit>7)){
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-            res = res*10 + digit;
-           i++;
-        }
-        return (int)(sign * res);
+        return helper(s , i , 0 , sign);
     }
 };
