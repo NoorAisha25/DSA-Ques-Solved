3372b15c9a13ddc36031f20dfebf06750211b9de
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-07-31 10:31:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.6 MB (43.24%)

--- 0410-split-array-largest-sum/solution.cpp
+++ 0410-split-array-largest-sum/solution.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    
-  
-    int func(vector<int>& arr, int k , int mid){
-        
-        int painter = 1;
-        int currSum = 0;
-        for(int i=0 ; i<arr.size() ; i++){
-            
-            currSum += arr[i];
-            if(currSum > mid ) {
-                painter++;
-                currSum = arr[i];
+    int pos(vector<int>& nums, int mid){
+
+        int subarrays = 1 ;
+        int cumSum = 0;
+
+        for(int i = 0 ; i<nums.size() ; i++){
+            cumSum += nums[i];
+
+            if(cumSum > mid){
+                subarrays++;
+                cumSum = nums[i];
             }
         }
-        return painter;
+        return subarrays;
     }
-    
-    int minTime(vector<int>& arr, int k) {
-        // code here
-        int l = *max_element(arr.begin() , arr.end());
-        int h = accumulate(arr.begin() , arr.end(),0);
+    int func(vector<int>& nums, int k){
+
+        int n = nums.size();
+        int l = *max_element(nums.begin() , nums.end());
+        int h = accumulate(nums.begin() , nums.end(), 0);
         int ans = -1;
-        
-        while( l<= h){
-            int mid  = l + (h-l)/2;
-            int PossibleAns = func(arr, k ,mid);
-            if(PossibleAns <= k){
+        while( l <= h){
+            
+            int mid = l + ( h - l)/2;
+            int possAns = pos(nums , mid);
+
+            if(possAns <= k){
                 ans = mid;
                 h = mid - 1;
             }
-            else{
-                l = mid + 1;
-            }
+            else l = mid + 1;
         }
-        return ans ;
-    }
+        return ans;
 
+    }
     int splitArray(vector<int>& nums, int k) {
-        return minTime(nums,k);
+        return func(nums, k);
     }
 };
