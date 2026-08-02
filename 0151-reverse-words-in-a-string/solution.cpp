c4d1fee000a01531dbada135883aad67ba0d4d71
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-02 10:09:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (69.91%)

--- 0151-reverse-words-in-a-string/solution.cpp
+++ 0151-reverse-words-in-a-string/solution.cpp
@@ -3,26 +3,28 @@ public:
     string reverseWords(string s) {
       reverse(s.begin() , s.end());
 
-      int i=0 ;  
-      int l = 0 , r = 0; //real hero
-      int n = s.length();
+      int l = 0 , r = 0;
+      int i = 0;
+      int n = s.size();
 
-      while(i < n ){
-            
-            while(i<n && s[i] != ' ') {
-                s[r] = s[i];
-                r++;
-                i++;
-            }
-            if( l < r){
-                reverse(s.begin()+l , s.begin()+r);
-                s[r] = ' ';
-                r++;
-                l = r;
-            }
+      while(i < n){
+
+        while(i<n && s[i] != ' '){
+            s[r] = s[i];
+            r++;
             i++;
+        }
+
+        if(l < r){
+            reverse(s.begin()+l , s.begin()+ r);
+            s[r] = ' ';
+            r++;
+            l = r;
+        }
+        i++;
       }
       s = s.substr(0 , r-1);
       return s;
+      
     }
 };
