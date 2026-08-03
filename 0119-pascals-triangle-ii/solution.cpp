ced4a9a38d77abf1d1d796a404e8a8a7f42b459c
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-03 13:23:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (48.86%)

--- 0119-pascals-triangle-ii/solution.cpp
+++ 0119-pascals-triangle-ii/solution.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> row(rowIndex+1 , 1);
-        long long ans=1;
-        for(int i=1 ; i<rowIndex ; i++){
-            ans = ans *(rowIndex - i+1)/i;
+        long long ans = 1;
+
+        for(int i = 1 ; i < rowIndex ; i++){
+            ans = ans * ( rowIndex-i + 1)/i;
             row[i] = ans;
         }
         return row;
