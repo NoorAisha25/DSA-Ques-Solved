6777131720c053dc8519124b5b03a886a1cbef94
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-05 16:29:36 +0000

    Sync LeetCode submission Runtime - 2 ms (49.96%), Memory - 8.5 MB (64.69%)

--- 0009-palindrome-number/solution.cpp
+++ 0009-palindrome-number/solution.cpp
@@ -3,13 +3,13 @@ public:
     bool isPalindrome(int x){
        long long temp = x;
        long long rev = 0;
-       if (x < 0) return false;
-       while( x > 0){
-          int d = x%10;
-          x = x/10;
-          rev= (rev * 10) + d;
-       }     
-       if( temp == rev) return true;
+       if(temp < 0) return false;
+       while(temp > 0){
+        int digit = temp%10;
+        temp = temp / 10;
+        rev = (rev * 10) + digit;
+       }
+       if(x == rev) return true;
        else return false;
     }
 };
