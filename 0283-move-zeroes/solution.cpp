25046e844e26e867580915fc4eeb133af40e080a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-06 14:17:25 +0000

    Sync LeetCode submission Runtime - 3 ms (14.36%), Memory - 23.9 MB (55.26%)

--- 0283-move-zeroes/solution.cpp
+++ 0283-move-zeroes/solution.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    void moveZeroes(vector<int>& nums) {
-          int n = nums.size();
+    void moveZeroes(vector<int>& arr) {
+          int n = arr.size();
         //   vector<int>temp;
-        //   int nz=0;
-        //   //step one
-        //   for(int i=0 ; i<n ; i++){
-        //     if(nums[i] != 0){
-        //         temp.push_back(nums[i]);
+
+        //   int nz= 0;
+        //   for(int i = 0 ; i<n ; i++){
+        //      if(arr[i] !=0 ){
+        //         temp.push_back(arr[i]);
         //         nz++;
-        //     }
+        //      }
         //   }
-        //   //step-2
-        //   for(int i=0 ; i<temp.size() ; i++){
-        //     nums[i] = temp[i];
+        //   for(int i = 0 ; i<temp.size() ; i++){
+        //     arr[i] = temp[i];
         //   }
-        //   //step-3
-        //   for(int i=nz ; i<n ; i++){
-        //     nums[i] = 0;
+        //   for(int i = nz ; i < n ; i++){
+        //      arr[i] = 0;
         //   }
 
-        int i=-1 ;
-        for(int j=0 ; j<n ; j++){
-            if(nums[j] != 0){
-                swap(nums[i+1] , nums[j]);
+        int i = -1;
+        for(int j = 0 ; j < n ; j++){
+            if(arr[j] != 0){
+                swap(arr[i+1] , arr[j]);
                 i++;
             }
         }
-
     }
 };
