f27ff52f83a4d06c17412631b649e8cd7090f81d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-06 14:50:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.8 MB (38.27%)

--- 0268-missing-number/solution.cpp
+++ 0268-missing-number/solution.cpp
@@ -1,42 +1,36 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        /*Better approach by hashing 
-        int n=nums.size();
-        vector<int>hash(n+1,0);
-        for(int i=0;i<n;i++){
-            hash[nums[i]]=1;
+        int n = nums.size();
+        // vector<int> hash(n+1 , 0);
+
+        // for(int i = 0 ; i<n ; i++){
+        //     hash[nums[i]] = 1;
+        // }
+
+        // for(int i = 0 ; i<=n ; i++){
+        //     if(hash[i] == 0){
+        //         return i;
+        //     }
+        // }
+        // return -1;
+
+        // int sum = 0;
+        // int s = n * (n+1)/2;
+
+        // for(int i = 0 ; i< n ; i++){
+        //     sum += nums[i];
+        // }
+        // return s-sum;
+        // return -1;
+
+        int xr = 0;
+        for(int i = 0 ; i<=n ; i++){
+            xr ^= i;
         }
-        for(int i=0;i<=n; i++){
-            if(hash[i]==0){
-                return i;
-            }
-        } return -1;
+        for(int i = 0 ; i<n ; i++){
+            xr ^= nums[i];
+        }
+        return xr;
     }
-    */
-     
-     int n=nums.size();
-    // int sum=(n*(n+1))/2;
-    // int s2=0;
-    // for(int i=0 ; i<n ; i++){
-    //          s2+= nums[i];
-    // }
-    // return (sum-s2);
-//practice
-    // vector<int> hash(n+1,0);
-    // for(int i=0 ;i<n ; i++){
-    //     hash[nums[i]]=1;
-    // }
-    // for(int i=0 ;i<n+1 ; i++){
-    //     if(hash[i]==0){
-    //         return i;
-    //     }
-    // }
-    //  
-     int sum = n*(n+1)/2;
-     int s2 = 0;
-     for(int i=0; i<n; i++){
-        s2 += nums[i];
-     } return (sum-s2);
-    return -1;}
 };
