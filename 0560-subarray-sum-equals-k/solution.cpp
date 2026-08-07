12b20f03ac53e3e25ac04fc25938971a08e9e00b
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-07 15:40:46 +0000

    Sync LeetCode submission Runtime - 47 ms (46.98%), Memory - 45.5 MB (38.51%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -14,21 +14,18 @@ public:
     //   }
     //   return cnt;
 
-       unordered_map<int , int> mp;
+       unordered_map<int,int>mp;
        mp.insert({0,1});
-       int sum = 0;
-       int cnt = 0;
-       for(int i = 0 ; i< n ; i++){
-           sum += nums[i];
-
-           if(mp.find(sum - k) != mp.end()){
-              cnt += mp[sum-k];
-           }
-
-           mp[sum]++;
+       int prefixsum = 0;
+       int res = 0;
+       for(int i = 0 ; i<n ; i++){
+          prefixsum += nums[i];
+          if(mp.find(prefixsum - k) != mp.end()){
+             res += mp[prefixsum - k];
+          }
+          mp[prefixsum]++;
        }
-       return cnt;
-
+       return res;
     }
 };
 
