73b7ede9096d56bfaa3d3c171d7ff8bae098e4f8
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-07 15:00:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.5 MB (98.69%)

--- 0136-single-number/solution.cpp
+++ 0136-single-number/solution.cpp
@@ -1,12 +1,22 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int n = nums.size();
-        int ans = 0 ;
+       int n = nums.size();
+    //    unordered_map<int , int>mp;
+    //    for(int i = 0 ; i<n ;i ++){
+    //        mp[nums[i]]++;
+    //    }
 
-        for(int i = 0 ; i<n ; i++){
-            ans ^= nums[i];
+    //    for(auto &it : mp){
+    //       if(it.second == 1){
+    //            return it.first;
+    //       }
+    //    }
+    //    return -1;
+        int xr = 0;
+        for(int i = 0 ; i< n ; i++){
+            xr ^= nums[i];
         }
-        return ans;
+        return xr;
     }
 };
