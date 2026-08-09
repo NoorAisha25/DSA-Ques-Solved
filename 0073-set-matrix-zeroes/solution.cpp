d8826a2aaf9715575914718b3903670b5037edbf
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-09 19:17:48 +0000

    Sync LeetCode submission Runtime - 3 ms (22.66%), Memory - 20.8 MB (96.01%)

--- 0073-set-matrix-zeroes/solution.cpp
+++ 0073-set-matrix-zeroes/solution.cpp
@@ -2,55 +2,52 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+         int n = matrix.size();
+         int m = matrix[0].size();
+
+         bool firrowim = false;
+         bool fircolim = false;
          
-         int n = matrix.size(); // -----row
-         int m = matrix[0].size();  // ----col
-
-         bool firrowimpa = false;
-         bool fircolimpa = false;
-         
-         //check if first row impacted
-         for(int col=0 ; col<m ; col++){
-                if(matrix[0][col] == 0){
-                   firrowimpa = true;
-                }   
+         //check 1st row
+         for(int col = 0 ; col < m ; col++){
+            if(matrix[0][col] == 0){
+                firrowim = true;
+            }
          }
-         //check if first col is impacted
-          for(int row=0 ; row<n ; row++){
-                if(matrix[row][0] == 0){
-                   fircolimpa = true;
-                }   
+         //fir col check
+         for(int row = 0 ; row < n ; row++){
+            if(matrix[row][0] == 0){
+                fircolim = true;
+            }
          }
 
-         //  banchmark set 
-         for(int i=1 ; i<n ; i++){
-            for(int j=1 ; j<m ; j++){
-               if(matrix[i][j] == 0){
-                matrix[i][0] =0;
-                matrix[0][j] =0;
-               }
-            }    
-         }
-
-         //excluding first row and first col
-         for(int i=1 ; i<n ; i++){
-            for(int j=1 ; j<m ; j++){
-                if( matrix[i][0] == 0 || matrix[0][j] == 0){
-                    matrix[i][j] =0;
+         //set markers
+         for(int i = 1  ; i < n ; i++){
+            for(int j = 1 ; j< m ; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
-            }    
+            }
          }
 
-         if(firrowimpa){
-            for(int j=0 ; j<m ; j++){
+         for(int i = 1 ; i < n ; i++){
+            for(int j = 1 ; j < m ; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
+                    matrix[i][j] = 0;
+                }
+            }
+         }
+
+         if(firrowim){
+            for(int j = 0 ; j < m ; j++){
                 matrix[0][j] = 0;
             }
          }
-         if(fircolimpa){
-            for(int i=0 ; i<n ; i++){
+         if(fircolim){
+            for(int i = 0 ; i < n ; i++){
                 matrix[i][0] = 0;
             }
-         }   
-    }
-    
+         }
+    } 
 };
