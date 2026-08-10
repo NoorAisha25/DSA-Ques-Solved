7297b47577c2d19a6a7463e3b6206b63f1a86c98
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-10 08:56:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.9 MB (41.97%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -1,44 +1,45 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n=nums.size();
-        int cnt1= 0, cnt2=0;
-        int maj1 = NULL,maj2 = NULL;
-        for(int i=0 ; i<n ;i++){
-            if(nums[i]==maj1){
+        int n = nums.size();
+        int cnt1 = 0 , cnt2 = 0;
+        int maj1 = NULL , maj2 = NULL;
+        for(int i = 0 ; i< n ; i++){
+            if(nums[i] == maj1){
                 cnt1++;
             }
-            else if(nums[i]==maj2){
+            else if(nums[i] == maj2){
                 cnt2++;
             }
-            else if(cnt1==0){
-                maj1=nums[i];
-                cnt1=1;
+            else if(cnt1 == 0 ){
+                maj1 = nums[i];
+                cnt1 = 1;
             }
-            else if(cnt2==0){
-                maj2=nums[i];
-                cnt2=1;
+            else if(cnt2 == 0 ){
+                maj2 = nums[i];
+                cnt2 = 1;
             }
             else{
                 cnt1--;
                 cnt2--;
             }
         }
-       /*---verification perpose--*/
-        vector<int>result;
-        int freq1 = 0;
-        int freq2 = 0;
-        for(int &num : nums){
-            if(num == maj1){
-                freq1++;
+
+        vector<int>res ;
+        int f1 = 0;
+        int f2 = 0;
+        for(int &it : nums){
+            if(it == maj1){
+                f1++;
             }
-            else if(num==maj2){
-                freq2++;
+            else if(it == maj2){
+                f2++;
             }
         }
-        if(freq1>floor(n/3)) result.push_back(maj1);
-        if(freq2>floor(n/3)) result.push_back(maj2);
 
-        return result;
+        if(f1 > floor(n/3)) res.push_back(maj1);
+        if(f2 > floor(n/3)) res.push_back(maj2);
+
+        return res;
     } 
 };
