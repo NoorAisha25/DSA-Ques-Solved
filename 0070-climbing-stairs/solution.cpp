213fdd7398c037de2b62c8404ca0d70531aee621
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-12 18:19:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (58.39%)

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int climbStairs(int n) {
-         //base case
          if( n <= 2) return n;
 
-         //dp+tabulation
-
-         int first = 1 ;
+         int first = 1;
          int second = 2;
-         for(int i = 3 ; i <= n ; i++){
+
+         for(int i = 2 ; i< n ; i++){
             int third = first + second;
             first = second;
             second = third;
