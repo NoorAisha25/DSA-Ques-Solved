c7aba351bf59f3b1b6df6304c26c786a798dcc99
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-12 18:15:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 97.5 MB (26.37%)

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,15 +1,14 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-       int n = prices.size();
-       int mini = prices[0];
-       int profit = 0;
-
-       for(int i = 1 ; i < n ; i++){
-          int cost = prices[i] - mini;
-          profit = max(profit , cost);
-          mini = min( mini , prices[i]);
-       }
-       return profit;
+    int maxProfit(vector<int>& arr) {
+      int n = arr.size();
+      int mini = arr[0];
+      int profit = 0;
+      for(int i = 1 ; i< n ; i++){
+        int cost = arr[i] - mini;
+        profit = max(profit , cost);
+        mini = min ( mini , arr[i]);
+      }
+      return profit;
     }
 };
