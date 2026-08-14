fe304139fe624428ca03d5c9313697c99fc9f420
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-14 10:34:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.7 MB (53.35%)

---
+++ 0213-house-robber-ii/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+       int n = nums.size();
+
+       if( n == 0) return 0;
+       if( n == 1) return nums[0];
+       if( n == 2) return max( nums[0] , nums[1]);
+
+       //exclude last element
+       vector<int> dp1(n);
+
+       dp1[0] = nums[0];
+       dp1[1] = max(nums[0] , nums[1]);
+
+       for(int i = 2 ; i < n - 1 ; i++){
+           dp1[i] = max( dp1[i-1] , dp1[i-2] + nums[i]);
+       } 
+       int case1 = dp1[n-2];
+
+       //exclude first element
+        vector<int> dp2(n);
+
+       dp2[1] = nums[1];
+       dp2[2] = max(nums[1] , nums[2]);
+
+       for(int i = 3 ; i < n ; i++){
+           dp2[i] = max( dp2[i-1] , dp2[i-2] + nums[i]);
+       } 
+       int case2 = dp2[n-1];
+
+       return max( case1 , case2);
+    }
+};
