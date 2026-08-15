107caa08be6cf31958b313f7c4a8d0574f069dba
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-15 15:43:29 +0000

    Sync LeetCode submission Runtime - 119 ms (5.09%), Memory - 162.4 MB (5.00%)

--- 0022-generate-parentheses/solution.cpp
+++ 0022-generate-parentheses/solution.cpp
@@ -1,46 +1,43 @@
 class Solution {
 public:
-    //global variable
-    vector<string> res;
+    vector<string>res;
+    
+    bool isvalid(string &s){
+        stack<int>st;
 
-    bool isvalid(string &curr){
-       stack<int> st;
-        for(char &ch : curr){
-
-            if(ch == '(') st.push(ch);
+        for(char c : s){
+            if(c == '(') st.push(c);
             else{
                 if(st.empty()) return false;
-                char c = st.top();
+                char ch = st.top();
                 st.pop();
-                if(ch == ')' && c != '(') return false;
+
+                if(c == ')' && ch != '(') return false;
             }
         }
         return st.empty();
     }
-    void solve (string& curr , int n){
+    void solve(string& s , int n){
 
-        // string stop generating when size becomes 2n na dchecking validity of string
-        if(curr.length() == 2*n){
-            if(isvalid(curr)){
-                res.push_back(curr);
+        if(s.size() == 2*n){
+            if(isvalid(s)){
+                res.push_back(s);
             }
             return;
         }
 
-        //first start with curr string as "("
-        curr.push_back('('); //do something
-        solve(curr , n);  // explore string combinations
-        curr.pop_back(); //undo (
+        s.push_back('(');
+        solve(s,n);
+        s.pop_back();
+        
+        s.push_back(')');
+        solve(s,n);
+        s.pop_back();
 
-        //2nd start with curr string a string )
-        curr.push_back(')'); // do
-        solve(curr, n);  //explore
-        curr.pop_back(); //undo
     }
     vector<string> generateParenthesis(int n) {
-        // using back tracking
-       string curr = ""; //---curr
-       solve(curr , n) ;///---explore
-       return res; //---final string 
+       string s = "";
+       solve( s , n);
+       return res; 
     }
 };
