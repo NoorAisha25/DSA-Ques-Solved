0bbd3d98cfc40b299eca5de92eea87d302f38181
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-15 15:31:17 +0000

    Sync LeetCode submission Runtime - 8 ms (83.66%), Memory - 19.9 MB (94.88%)

--- 3453-generate-binary-strings-without-adjacent-zeros/solution.cpp
+++ 3453-generate-binary-strings-without-adjacent-zeros/solution.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-        void helperfun(int i , string &s , vector<string>& ans){
+        
+    void helper(int i , string &s , vector<string>&ans){
+        if(i >= s.size()){
+            ans.push_back(s);
+            return;
+        }
 
-            if(i >= s.size()){
-                ans.push_back(s);
-                return ;
-            }
-            
-            
-            
-            helperfun(i+1 , s , ans);
-            
-            s[i] = '0';
-            
-            helperfun(i+2, s , ans);
-            s[i] = '1';
-        } 
-
+        helper(i + 1 , s , ans);
+        s[i] = '0';
+        helper(i+2 , s , ans);
+        s[i] = '1';
+    }    
     vector<string> validStrings(int n) {
-         string s(n , '1');
-  
-        vector<string> ans;
-        
-        helperfun(0 , s , ans);
-        
+        string s(n , '1');
+        vector<string>ans;
+
+        helper( 0 , s , ans);
         return ans;
     }
 };
