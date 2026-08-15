942159132e65451e6020e7fcbaeb5cd16db045c2
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-15 18:18:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.3 MB (84.69%)

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void solve(int i , vector<int>& nums , vector<vector<int>>&ans , vector<int>& comb ){
+    
+    void solve( int i , vector<vector<int>> &ans , vector<int>&arr , vector<int>&comb){
 
-            ans.push_back(comb);
+        ans.push_back(comb);
 
-        for(int j = i ; j<nums.size() ; j++){
-            if(j>i && nums[j] == nums[j-1]) continue;
-            comb.push_back(nums[j]);
-            solve(j + 1 ,nums, ans , comb );
+        for(int j = i ; j < arr.size() ; j++){
+            if(j > i && arr[j] == arr[j-1]) continue;
+
+            comb.push_back(arr[j]);
+            solve(j+1 , ans , arr ,comb);
             comb.pop_back();
         }
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> comb;
-        int n = nums.size();
-        sort(nums.begin() , nums.end());
-        solve(0 , nums, ans , comb );
-        return ans;
+       
+       vector<vector<int>> ans;
+       vector<int>comb ; 
+       sort(nums.begin() , nums.end());
+
+       solve(0 , ans , nums , comb);
+
+       return ans;
     }
 };
