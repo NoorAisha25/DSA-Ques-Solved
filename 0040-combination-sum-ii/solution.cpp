78720c46f5aed8e99a71441f37f6324a13ab8081
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-15 17:29:43 +0000

    Sync LeetCode submission Runtime - 4 ms (21.62%), Memory - 14.1 MB (39.00%)

--- 0040-combination-sum-ii/solution.cpp
+++ 0040-combination-sum-ii/solution.cpp
@@ -1,29 +1,34 @@
 class Solution {
 public:
 
-    void solve(int i , vector<int>&arr, vector<int>&comb , vector<vector<int>> &ans , int k){
-       
-       if( k < 0){
-        return ;
-       }
-       if(k == 0){
+    void solve(int i ,  vector<vector<int>> &ans , vector<int>&arr , int k , vector<int>&comb ){
+
+        if( i > arr.size() || k < 0 ){
+            return;
+        }
+        if(k == 0){
             ans.push_back(comb);
             return;
-       }
-        for (int j = i; j < arr.size(); j++) {
-            if( j > i && arr[j] == arr[j-1]) continue;
-
-            comb.push_back(arr[j]);
-            solve(j + 1, arr,comb , ans ,k - arr[j]); 
-            comb.pop_back();
         }
+        
+        for(int j = i ; j < arr.size() ; j++){
+
+            if(j > i && arr[j] == arr[j-1]) continue;
+            //include
+            comb.push_back(arr[j]);
+            solve(j+1 , ans , arr, k-arr[j] , comb);
+            //exclude
+            comb.pop_back();
+      }
     }
+  
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
        
-        vector<vector<int>> ans;
-        vector<int> comb;
         sort(arr.begin() , arr.end());
-        solve( 0 , arr, comb , ans , target);
+        vector<vector<int>>ans;
+        vector<int>comb;
+
+        solve(0 , ans , arr , target , comb);
         return ans;
     }
 };
