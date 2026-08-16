68de349f43f24dc88a064855ee8292100c570719
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-16 10:00:22 +0000

    Sync LeetCode submission Runtime - 1 ms (15.15%), Memory - 11.9 MB (53.81%)

---
+++ 0063-unique-paths-ii/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int solve(int i , int j , int m , int n ,vector<vector<int>>&t, vector<vector<int>>& arr ){
+
+        if(i <0 || i >=m || j < 0 || j>=n ||  arr[i][j] == 1){
+            return 0 ;
+        }
+
+        if( i == m-1 && j == n-1){
+            return 1;
+        }
+        
+        if( t[i][j] != -1) return t[i][j];
+
+        int right = solve( i , j + 1 , m , n , t , arr);
+        int down = solve( i+ 1 , j , m , n , t , arr);
+
+        return t[i][j] = right + down;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& arr) {
+        int m = arr.size();
+        int n = arr[0].size();
+        vector<vector<int>> t(m+1 , vector<int>(n+1 , -1));
+
+        return solve( 0 , 0 , m , n , t , arr);
+    }
+};
