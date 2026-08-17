5292d0a5c2d74a5a4ed7b2a9d1c5493579d7b760
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-17 18:08:56 +0000

    Sync LeetCode submission Runtime - 6 ms (29.62%), Memory - 10.6 MB (99.21%)

--- 0402-remove-k-digits/solution.cpp
+++ 0402-remove-k-digits/solution.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        string result = "";
-        int n = num.length();
-        for(int i = 0 ; i<n ; i++){
-           
-           while(result.length() > 0 && k > 0 && result.back() > num[i] ) {
-            result.pop_back();
-            k--;
+        string res = "";
+
+        int n = num.size();
+
+        for(int i = 0 ; i<n ; i++){  // for loop chalao string par jo input de rakha hai
+           while(res.length() >0 && k>0 && res.back() > num[i]){ // res m hamein pop karna hai tab jab res khali na ho + k zero k barabar na ho  + res m jo element hai woh input k element se barabar hai
+              res.pop_back();
+              k--;
            }
-           if(result.length() > 0 || num[i] != '0') {
-               result.push_back(num[i]);
+           //push tab karna hai jab res m element mojood hai ya phit element hamara 0 se nhi shuru ho raha hai
+           if(res.length() > 0 || num[i] != '0'){
+             res.push_back(num[i]);
            }
         }
-            while( k > 0 && result.length() > 0){
-               result.pop_back();
-               k--;
+            // ye us condition k liye hai jaise input = 12345 isme saare prev ele successor se chote hain th yahan bas k-- jab tak k zero k barabar na ho jaye
+            while(k > 0 && res.length() > 0 ){
+                res.pop_back();
+                k--;
             }
-           
-           if(result == ""){
-            return "0";
-           }
-
-           return result;
-    }
+             // agar end m result empty hai toh 0 return kar do
+            if(res == "") return "0";
+            //warna res jo aaya hai woh return kar do
+            return res;
+    }    
 };
