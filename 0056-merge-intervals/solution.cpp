0f8ac90e51f209a714812c897c61cc581f106ea6
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-19 18:40:35 +0000

    Sync LeetCode submission Runtime - 3 ms (89.34%), Memory - 23.8 MB (60.45%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
-       vector<vector<int>> res;
+         vector<vector<int>>ans;
 
-       sort(arr.begin() , arr.end());
+         sort(arr.begin() , arr.end());
 
-       res.push_back(arr[0]); // 1st interval of intervals added in result to compare 
-       int i = 1;
-       while( i < arr.size()){
-         //overlapping condition
-         if(arr[i][0] <= res.back()[1]){
-              res.back()[1] = max(arr[i][1] , res.back()[1]);
+         ans.push_back(arr[0]);
+
+         int i = 1;
+
+         while( i  < arr.size()){
+
+            if( arr[i][0] <= ans.back()[1]){
+                ans.back()[1] = max( ans.back()[1] , arr[i][1]);
+            }
+
+            else{
+                ans.push_back(arr[i]);
+            }
+            i++;
          }
-         //non overlapping condition
-         else{
-              res.push_back(arr[i]);
-         }
-           i++;
-       }
-       return res;
+         return ans;
     }
 };
