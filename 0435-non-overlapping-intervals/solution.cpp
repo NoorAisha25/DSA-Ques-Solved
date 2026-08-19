baf118dbc0376854c67dfe4b2a043c6449c1d098
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-19 18:46:06 +0000

    Sync LeetCode submission Runtime - 40 ms (76.62%), Memory - 94.1 MB (21.47%)

--- 0435-non-overlapping-intervals/solution.cpp
+++ 0435-non-overlapping-intervals/solution.cpp
@@ -1,22 +1,24 @@
 class Solution {
 public:
     int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        //sort in inc order of end of each intervals
+
         sort(intervals.begin() , intervals.end() , [](vector<int>&A , vector<int>&B){
-               return A[1] < B[1];
+            return A[1] < B[1];
         });
 
         int cnt = 0;
-        int preEnd = intervals[0][1];
-
+        vector<vector<int>>ans;
+        ans.push_back(intervals[0]);
+        
         int i = 1;
-        while( i < intervals.size()){
-            //overlapping condn
-            if(intervals[i][0] < preEnd){
+
+        while(i < intervals.size()){
+
+            if(intervals[i][0] < ans.back()[1]){
                 cnt++;
             }
             else{
-                preEnd = intervals[i][1];
+                ans.push_back(intervals[i]);
             }
             i++;
         }
