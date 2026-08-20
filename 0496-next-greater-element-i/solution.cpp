89674cd279fb21f4d934778c7aa6da7c59ccc9e2
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-20 12:33:56 +0000

    Sync LeetCode submission Runtime - 2 ms (47.31%), Memory - 12.8 MB (37.99%)

--- 0496-next-greater-element-i/solution.cpp
+++ 0496-next-greater-element-i/solution.cpp
@@ -1,14 +1,11 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size();
-        int m = nums2.size();
+        int n = nums2.size();
+        unordered_map<int,int>mp;
+        stack<int>st;
 
-        unordered_map<int , int> mp ;
-        stack<int> st;
-
-        for(int i = m-1 ; i >= 0 ; i--){
-
+        for(int i = n-1 ; i >= 0 ; i--){
             while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
@@ -16,17 +13,20 @@ public:
             if(st.empty()){
                 mp[nums2[i]] = -1;
             }
-            else if(!st.empty()){
+            else{
                 mp[nums2[i]] = st.top();
             }
             st.push(nums2[i]);
         }
 
-        vector<int> res;
+        vector<int> ans;
 
-        for(int i = 0 ; i<n ; i++){
-            res.push_back(mp[nums1[i]]);
+        int m = nums1.size();
+
+        for(int i = 0 ; i < m ; i++){
+            ans.push_back(mp[nums1[i]]);
         }
-        return res;
+
+        return ans;
     }
 };
