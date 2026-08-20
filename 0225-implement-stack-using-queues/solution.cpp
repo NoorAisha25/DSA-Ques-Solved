eddc3f636fb44599fd2dc690e695e1a584b25e3e
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-20 07:58:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (60.67%)

--- 0225-implement-stack-using-queues/solution.cpp
+++ 0225-implement-stack-using-queues/solution.cpp
@@ -1,30 +1,30 @@
 class MyStack {
     private:
-     queue<int>q;
+       queue<int>q;
     public:
-    MyStack() {
-
-    }
+    MyStack() {}
     
     void push(int x) {
-        int s = q.size();
+        int n = q.size();
         q.push(x);
-        for(int i=0 ; i<s ; i++){
+        for(int i = 0 ; i<n ; i++){
             q.push(q.front());
             q.pop();
         }
     }
     
     int pop() {
-        if(q.empty()) return -1;
-        int val = q.front();
-        q.pop();
-        return val;
+       if(q.empty()) return -1;
+       else{
+          int val = q.front();
+          q.pop();
+          return val;
+       }
     }
     
     int top() {
-        if(!q.empty()) return q.front();
-        return -1;
+        if(q.empty()) return -1;
+        return q.front();
     }
     
     bool empty() {
