c3aff54d5f61bc512f5305b42988507e7c594387
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-20 16:59:56 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 21.6 MB (77.59%)

--- 0735-asteroid-collision/solution.cpp
+++ 0735-asteroid-collision/solution.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
-        vector<int> st;
+        vector<int>st;
 
-        for(int &a : asteroids){
-            
-            //collision ki condition
-            while(!st.empty() && a<0 && st.back() > 0){
+        for(auto &a : asteroids){
+            while(!st.empty() && a < 0 && st.back() > 0){
 
                 int sum = a + st.back();
 
-                if(sum < 0){
+                if(sum < 0){ 
                     st.pop_back();
                 }
                 else if(sum > 0){
                     a = 0;
                 }
                 else{
-                    // sum == 0
                     st.pop_back();
                     a = 0;
                 }
             }
-            if(a != 0){
+            if( a != 0){
                 st.push_back(a);
             }
         }
