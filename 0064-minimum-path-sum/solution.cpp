3cdad9106241abaa83b350d18cf0690c3b978c9d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-21 11:10:52 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.4 MB (15.63%)

--- 0064-minimum-path-sum/solution.cpp
+++ 0064-minimum-path-sum/solution.cpp
@@ -4,17 +4,19 @@ public:
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> t ( m , vector<int>(n , 0));
+        vector<vector<int>>t(m , vector<int>(n , 0));
 
         t[0][0] = grid[0][0];
-        
+
         for(int col = 1 ; col < n ; col++){
             t[0][col] = grid[0][col] + t[0][col-1];
         }
+
         for(int row = 1 ; row < m ; row++){
             t[row][0] = grid[row][0] + t[row-1][0];
         }
-        for(int i = 1 ; i< m ; i++){
+
+        for(int i = 1 ; i < m ; i++){
             for(int j = 1 ; j < n ; j++){
                 t[i][j] = grid[i][j] + min(t[i-1][j] , t[i][j-1]);
             }
