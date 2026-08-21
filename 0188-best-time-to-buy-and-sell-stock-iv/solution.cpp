091e849bad447ce8d6ccd8f60e7ba91f015e2af9
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-21 09:15:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.3 MB (86.72%)

--- 0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/solution.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        vector<int> buy( k + 1 , INT_MIN);
-        vector<int> sell( k + 1 , 0);
+        vector<int>buy(k+1 , INT_MIN);
+        vector<int>sell(k+1 , 0);
 
-        for(int price : prices){
-            for( int t = 1 ; t <= k ; t++){
-                buy[t] = max( buy[t] , sell[t-1] - price);
-                sell[t] = max( sell[t] , buy[t] + price);
+        for(int i = 0 ; i <n ; i++){
+            for(int t = 1 ; t <= k ; t++){
+                buy[t] = max( buy[t] , sell[t-1] - prices[i]);
+                sell[t] = max( sell[t] , buy[t] + prices[i]); 
             }
         }
         return sell[k];
