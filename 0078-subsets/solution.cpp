a711e27f4c01589d60d40e732062eb26b647d898
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-24 18:15:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (38.23%)

--- 0078-subsets/solution.cpp
+++ 0078-subsets/solution.cpp
@@ -3,16 +3,15 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         int subset = 1 << n;
-        vector<vector<int>> ans;
 
-        for(int i = 0; i < subset ; i++){
-            vector<int>temp;
+        vector<vector<int>>ans;
+
+        for(int i = 0 ; i < subset ; i++){
+            vector<int>list;
             for(int j = 0 ; j < n ; j++){
-                if( i & (1 << j)){
-                    temp.push_back(nums[j]);
-                }
+                if( i & (1 << j)) list.push_back(nums[j]);
             }
-            ans.push_back(temp);
+            ans.push_back(list);
         }
         return ans;
     }
