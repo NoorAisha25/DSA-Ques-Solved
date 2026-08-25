06815374fdba338abfc3b1cbde412784af22233f
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-25 07:05:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.8 MB (96.99%)

--- 0260-single-number-iii/solution.cpp
+++ 0260-single-number-iii/solution.cpp
@@ -1,32 +1,22 @@
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        // unordered_map<int,int>mp;
-
-        // for(int i =0 ; i< nums.size() ; i++){
-        //     mp[nums[i]]++;
-        // }
-
-        // vector<int>ans;
-        // for(auto it : mp){
-        //     if(it.second == 1) ans.push_back(it.first);
-        // }
-        // return ans;
-
-        long long x = 0;
-        for(int i = 0 ; i<nums.size() ; i++){
+        int n = nums.size();
+        long x = 0;
+        for(int i = 0 ; i < n ; i++){
             x ^= nums[i];
         }
 
-        int mask = (x & (-x));
-        int b1 = 0 ;
-        int b2 = 0;
-
-        for(int i = 0 ; i<nums.size() ; i++){
-
-            if(mask & nums[i]) b1 ^= nums[i];
-            else b2 ^= nums[i];
+        long mask = (x & (-x));
+        int a = 0 ; int b = 0;
+        for(int i : nums){
+            if(mask & i){
+                b ^= i;
+            }
+            else{
+                a ^= i;
+            }
         }
-        return {b1 , b2};
+        return {a , b};
     }
 };
