ceb38430509dfdf6cd6aabfc02232c0cc09bbc09
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-25 08:47:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.1 MB (12.56%)

--- 0014-longest-common-prefix/solution.cpp
+++ 0014-longest-common-prefix/solution.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& s) {
-        
-        if( s.empty()) return "";
+        string res = "";
         sort(s.begin() , s.end());
-        string first = s.front();
-        string last = s.back();
-        string prefix = "";
-        
 
-        for(int i = 0 ; i < first.size() && i < last.size() ; i++){
-            if(first[i] == last[i]){
-                prefix.push_back(first[i]);
+        string f = s.front();
+        string l = s.back();
+
+        for(int i = 0 ; i < f.size() && i < l.size() ; i++){
+            if(f[i] == l[i]){
+                res.push_back(f[i]);
             }
-            else break;
-        } 
-
-        return prefix;
+            else{
+                break;
+            }
+        }
+        return res;
     }
 };
