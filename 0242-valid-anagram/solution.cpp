f798fc6266773721f9ad9b8ccce0cc83ccf9fb8a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-25 09:08:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (11.14%)

--- 0242-valid-anagram/solution.cpp
+++ 0242-valid-anagram/solution.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        // int n = s.length();
-        // int m = t.length();
-
-        // if(n != m) return false;
-        // sort(s.begin() , s.end());
-        // sort(t.begin() , t.end());
-        // return s == t;
+        if(s.size() != t.size()) return false;
 
         unordered_map<char,int>mp1;
         unordered_map<char,int>mp2;
 
-        for(char c : s){
+        for(char &c : s){
             mp1[c]++;
         }
-        for(char c : t){
+        for(char &c : t){
             mp2[c]++;
         }
-
         return mp1 == mp2;
     }
 };
