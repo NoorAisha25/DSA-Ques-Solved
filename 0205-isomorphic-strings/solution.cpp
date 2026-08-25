426cc48c44e3ae74b452772a79d7792b4df10567
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-25 08:54:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.4 MB (57.29%)

--- 0205-isomorphic-strings/solution.cpp
+++ 0205-isomorphic-strings/solution.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int f = s.length();
-        int n = t.length();
+        if(s.size() != t.size()) return false;
 
-        if( s.length() != t.length()) return false;
+        vector<int> mps(256 , -1);
+        vector<int> mpt(256 , -1);
 
-        vector<int> mapS(256 , -1);
-        vector<int> mapT(256, -1);
+        int n = s.size();
 
-        for(int i = 0 ; i< n ; i++ ){
-
-            char c1 = s[i];
-            char c2 = t[i];
-
-            if(mapS[c1] == -1  && mapT[c2] == -1){
-                mapS[c1] = c2;
-                mapT[c2] = c1;
+        for(int i = 0 ; i < n ; i++){
+            if(mps[s[i]] == -1 && mpt[t[i]] == -1){
+                mps[s[i]] = t[i];
+                mpt[t[i]] = s[i];
             }
-
-            else if (mapS[c1] != c2 && mapT[c2] != c1){
+            else if( mps[s[i]] != t[i] && mpt[t[i]] != s[i]){
                 return false;
             }
-
         }
         return true;
     }
