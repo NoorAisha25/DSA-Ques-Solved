f964232edf55026a4d301a53a33a4d01d53331ac
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-25 09:01:51 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.1 MB (23.26%)

--- 0812-rotate-string/solution.cpp
+++ 0812-rotate-string/solution.cpp
@@ -1,22 +1,12 @@
 class Solution {
 public:
-    bool rotateString(string s, string goal) {
-        int m = s.length();
-        int n = goal.length();
-        
-        if(m != n) return false;
+    bool rotateString(string s, string g) {
+        int n = s.size();
 
-        // for(int cnt = 1 ; cnt <= m ; cnt++){
-        //     //shift rotate one 
-        //     rotate(s.begin() , s.begin()+1, s.end());
-        //     if(s == goal) return true;
-        // }
-        // return false;
-
-        if((s+s).find(goal) != string :: npos) 
-        {
-            return true;
+        if(s.size() != g.size()){
+            return false;
         }
-        else return false;
+        if((s+s).find(g) != string :: npos) return true;
+        return false;
     }
 };
