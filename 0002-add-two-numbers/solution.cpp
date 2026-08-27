89c596a26a91d31a51fe108cd7c10762a8e67d6d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-27 13:14:48 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 77.2 MB (12.76%)

--- 0002-add-two-numbers/solution.cpp
+++ 0002-add-two-numbers/solution.cpp
@@ -11,8 +11,8 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+       if(l1 == NULL || l2 == NULL) return NULL;
 
-       if(l1 == NULL && l2 == NULL) return NULL;
        ListNode* temp1 = l1;
        ListNode* temp2 = l2;
        ListNode* dummy = new ListNode(-1);
@@ -20,25 +20,24 @@ public:
        int carry = 0;
 
        while( temp1 != NULL || temp2 != NULL){
+           int sum = carry;
 
-            int sum = carry;
-            if(temp1) sum = sum + temp1->val;
-            if(temp2) sum = sum + temp2->val;
+           if(temp1) sum = sum + temp1->val;
+           if(temp2) sum = sum + temp2->val;
+        
+           ListNode* newNode = new ListNode(sum % 10);
+           carry = sum / 10;
+           curr -> next = newNode;
+           curr = newNode;
 
-            ListNode* NewNode = new ListNode(sum % 10);
-            carry = sum / 10;
-            curr->next = NewNode ;
-            curr = curr->next;
-
-            if(temp1) temp1 = temp1 -> next;
-            if(temp2) temp2 = temp2 -> next;
+           if(temp1) temp1 = temp1->next;
+           if(temp2) temp2 = temp2->next;
        }
 
-       if(carry) {
-        ListNode* nN = new ListNode(carry);
-        curr -> next = nN;
+       if(carry){
+          ListNode* n = new ListNode(carry);
+          curr -> next = n;
        }
-
-       return dummy->next;
+        return dummy->next;
     }
 };
