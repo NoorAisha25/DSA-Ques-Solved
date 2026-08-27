35247edb88eab07c6f3540c8ae084857cc315c84
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-27 09:28:53 +0000

    Sync LeetCode submission Runtime - 24 ms (6.48%), Memory - 15 MB (5.63%)

--- 0141-linked-list-cycle/solution.cpp
+++ 0141-linked-list-cycle/solution.cpp
@@ -9,29 +9,14 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        /*------Naive approach in which TC- O(nlogn or n*2*1)----*/
-        // map<ListNode* , int> mpp;
-        // ListNode* temp = head;
+      map<ListNode* , int>mp;
+      ListNode* temp = head;
 
-        // while( temp!= NULL){
-
-        //     if(mpp.find(temp) != mpp.end()) return true;
-        //     mpp[temp] = 1;
-        //     temp = temp->next;
-        // }
-        // return false;
-
-        /* ----using tortoisehare problem -----*/
-
-        ListNode* slow = head;
-        ListNode* fast = head;
-
-        while(fast != NULL && fast->next != NULL){
-               slow = slow->next;
-               fast = fast->next->next;
-
-               if(slow == fast) return true;
-        }
-        return false;
+      while( temp != NULL){
+          if(mp.find(temp) != mp.end()) return true;
+          mp[temp] = 1;
+          temp = temp->next;
+      }
+      return false;
     }
 };
