bcd24cfe98946576f50e7af33a3a6f9a2d81acd8
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-27 11:06:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.9 MB (92.66%)

--- 0019-remove-nth-node-from-end-of-list/solution.cpp
+++ 0019-remove-nth-node-from-end-of-list/solution.cpp
@@ -11,57 +11,25 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
+         if(head == NULL || head -> next == NULL) return NULL;
+         ListNode* fast = head;
+         for(int i = 0 ; i < n ; i++){
+            fast = fast -> next;
+         }
 
-        if(head == NULL || head->next == NULL) return NULL;
-        // int cnt = 0;
-        // ListNode* temp = head;
+         ListNode* slow = head;
 
-        // while(temp != NULL){
-        //     cnt++;
-        //     temp = temp->next;
-        // }
-
-        // int pos = cnt - n;
-
-        
-        // while( pos == 0){
-        //     ListNode* delnode = head;
-        //     head = head->next;
-        //     delete delnode;
-        //     return head;
-        // }
-
-        // temp = head ;
-
-        // for(int i=1 ; i<pos ; i++){
-        //     temp = temp->next;
-        // }
-
-        // ListNode* delnode = temp->next;
-        // temp->next = temp->next->next;
-        // delete delnode;
-
-        ListNode* fast = head;
-        for(int i=0 ; i<n ; i++) fast = fast->next;
-
-        ListNode* slow = head;
-
-        if(fast == NULL) {
-            // ListNode* temp = head;
-            // head = head->next;
-            // delete temp;
-            // return head;
-            return head->next;
+         if(fast == NULL) {
+            return head -> next;
         }
+         while(fast -> next != NULL){
+            slow = slow ->next;
+            fast = fast -> next;
+         }
 
-        while(fast->next != NULL){
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        ListNode* deln = slow->next;
-        slow->next = slow->next->next;
-        delete deln;
-        return head;
+         ListNode* d = slow->next;
+         slow->next = slow->next->next;
+         delete d;
+         return head;
     }
 };
