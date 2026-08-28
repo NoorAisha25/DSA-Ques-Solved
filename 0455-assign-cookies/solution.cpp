42a0d0562f7c38c6ed5ae22fc5258a6ffd69de99
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-28 16:43:14 +0000

    Sync LeetCode submission Runtime - 7 ms (71.23%), Memory - 44.7 MB (97.19%)

--- 0455-assign-cookies/solution.cpp
+++ 0455-assign-cookies/solution.cpp
@@ -4,18 +4,19 @@ public:
         int m = g.size();
         int n = s.size();
 
-        int i = 0;
-        int j = 0;
-
-        sort(s.begin()  ,s.end());
+        sort(s.begin() , s.end());
         sort(g.begin() , g.end());
 
-        while( i < n && j < m ){
+        int i = 0 ;
+        int j = 0 ;
+
+        while( i < n && j < m){
             if(s[i] >= g[j]){
                 j++;
             }
             i++;
         }
+
         return j;
     }
 };
