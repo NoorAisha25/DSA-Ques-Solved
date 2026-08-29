6fee87c1bf09f2c7a317dfc9de2a1472cf358403
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-29 15:39:05 +0000

    Sync LeetCode submission Runtime - 3 ms (2.17%), Memory - 18.4 MB (73.04%)

--- 0101-symmetric-tree/solution.cpp
+++ 0101-symmetric-tree/solution.cpp
@@ -12,14 +12,14 @@
 class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
-        if (!root) return true;
-        return isMirror(root->left, root->right);
+         if (root == NULL) return true;
+         return isMirror(root -> left , root -> right);
        }
 private:
-    bool isMirror(TreeNode* t1, TreeNode* t2) {
-        if (!t1 && !t2) return true;         
-        if (!t1 || !t2) return false;        
-        if (t1->val != t2->val) return false; 
-        return isMirror(t1->left, t2->right) && isMirror(t1->right, t2->left);       
+    bool isMirror(TreeNode* p , TreeNode* q){
+        if( p == NULL && q == NULL ) return true;
+        if(p == NULL || q == NULL ) return false;
+        if(p -> val != q -> val) return false;
+        return isMirror(p -> left , q -> right) && isMirror(p -> right , q -> left);
     }
 };
