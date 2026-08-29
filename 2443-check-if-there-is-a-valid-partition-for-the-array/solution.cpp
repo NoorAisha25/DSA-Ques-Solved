6d3069afc85f93c70d668dc4ec5b6496ba25af9b
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-29 11:00:31 +0000

    Sync LeetCode submission Runtime - 18 ms (11.54%), Memory - 97.2 MB (33.11%)

--- 2443-check-if-there-is-a-valid-partition-for-the-array/solution.cpp
+++ 2443-check-if-there-is-a-valid-partition-for-the-array/solution.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    bool solve(int i , vector<int>&nums , vector<int>&t){
-        int n = nums.size();
-
-        if(i == n) return true;
+    bool solve(int i  , vector<int>&arr , vector<int>&t ){
+        int n = arr.size();
+        if(i == arr.size()) return true;
 
         bool res = false;
-        //memoization checks the duplicate entry
+
         if(t[i] != -1) return t[i];
-        //1st condition -- 2 elements equal
-        if( i+1 < n && nums[i] == nums[i+1]) res |= solve( i+2 , nums , t);
-        //2nd condition -- 3 elements equal
-        if(i+2 < n && nums[i] == nums[i+1] && nums[i+1] == nums[i+2]) res |= solve( i + 3 , nums, t);
-        //3rd condn -- exactly consecutive elements are equal
-        if(i+2 < n && nums[i+1] - nums[i] == 1 && nums[i+2] - nums[i+1] == 1) res |= solve( i + 3 , nums , t);
+
+        if(i+1 < n  && arr[i] == arr[i+1]){
+            res |= solve(i+2 , arr , t);
+        }
+
+        if(i+2 <n && arr[i] == arr[i+1] && arr[i+1] == arr[i+2]){
+            res |= solve(i+3 , arr , t);
+        }
+
+        if(i+2 < n && arr[i+1] - arr[i] == 1 && arr[i+2] - arr[i+1] == 1){
+            res |= solve(i+3 , arr , t);
+        }
 
         return t[i] = res;
     }
     bool validPartition(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>t(n , -1);
-
-        return solve(0 , nums , t);
+       int n = nums.size();
+       vector<int>t(n , -1);
+       return solve(0 , nums , t);
     }
 };
