dae268a63d88196917b3f24490f7bd06e3fbb5a9
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-30 15:32:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50.3 MB (33.25%)

--- 0485-max-consecutive-ones/solution.cpp
+++ 0485-max-consecutive-ones/solution.cpp
@@ -4,13 +4,14 @@ public:
        int n = nums.size();
        int maxi = 0;
        int cnt = 0;
-       for(int i = 0 ; i< n ; i++){
-           if(nums[i] == 1){
-               cnt++;
-               maxi = max(maxi , cnt);
-           }
-           else cnt = 0;
+
+       for(int i = 0 ; i < n ; i++){
+          if(nums[i] == 1){
+            cnt++;
+            maxi = max(maxi , cnt);
+          }
+          else cnt =  0;
        }
-          return maxi;
+       return maxi;
     }
 };
