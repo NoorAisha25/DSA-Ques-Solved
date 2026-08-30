dc21f849c241b8e46753dc1e0aa87b6a33501563
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-30 18:51:42 +0000

    Sync LeetCode submission Runtime - 3 ms (85.42%), Memory - 128.3 MB (54.31%)

--- 2271-rearrange-array-elements-by-sign/solution.cpp
+++ 2271-rearrange-array-elements-by-sign/solution.cpp
@@ -3,20 +3,19 @@ public:
     vector<int> rearrangeArray(vector<int>& nums) {
       int n = nums.size();
       vector<int>res(n);
-      
       int pi = 0;
       int ni = 1;
-      for(int i = 0 ; i< n ; i++){
-         if(nums[i] > 0){
-            res[pi] = nums[i];
-            pi += 2;
-         }
-         else{
+
+      for(int i = 0 ; i < n ; i++){
+        if(nums[i] < 0){
             res[ni] = nums[i];
             ni += 2;
-         }
+        }
+        else{
+            res[pi] = nums[i];
+            pi += 2;
+        }
       }
       return res;
-
     }
 };
