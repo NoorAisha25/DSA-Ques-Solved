b7c946912768b4a05238ffcd6ea705f5e2488013
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-30 10:09:09 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.4 MB (19.86%)

--- 1878-check-if-array-is-sorted-and-rotated/solution.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/solution.cpp
@@ -3,9 +3,10 @@ public:
     bool check(vector<int>& arr) {
         int n = arr.size();
         int cnt = 0;
-        for(int i = 0 ; i< n ; i++){
-
-            if(arr[i] > arr[(i+1) % n]) cnt++;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] > arr[(i+1) %n]){
+                cnt++;
+            }
         }
         if(cnt <= 1) return true;
         else return false;
