e4a671ffcb09d4b6943227aa89ae448ec77b5661
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-30 10:20:24 +0000

    Sync LeetCode submission Runtime - 5 ms (26.33%), Memory - 263 MB (60.67%)

--- 0189-rotate-array/solution.cpp
+++ 0189-rotate-array/solution.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> temp;
-        k = k % n;      // here remain alert
-        for(int i = n-k ; i< n ; i++){ // here choose wisely
+        k = k % n;
+        vector<int>temp;
+
+        for(int i = n-k ; i < n ; i++){
             temp.push_back(nums[i]);
         }
-        for(int i = n-1 ; i >= k ; i -- ){
+
+        for(int i = n - 1 ; i >= k ; i--){
             nums[i] = nums[i - k];
         }
+
         for(int i = 0 ; i < k ; i++){
             nums[i] = temp[i];
         }
