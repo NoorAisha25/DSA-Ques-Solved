a6da2bd2322084a3604a4e1514a09971548a8b5a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-30 18:43:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 23.7 MB (75.08%)

--- 0229-majority-element-ii/solution.cpp
+++ 0229-majority-element-ii/solution.cpp
@@ -2,44 +2,46 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-        int cnt1 = 0 , cnt2 = 0;
-        int maj1 = NULL , maj2 = NULL;
-        for(int i = 0 ; i< n ; i++){
-            if(nums[i] == maj1){
+        int cnt1 = 0;
+        int cnt2 = 0;
+        int majEle1 = NULL;
+        int majEle2 = NULL;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(majEle1 == nums[i]){
                 cnt1++;
             }
-            else if(nums[i] == maj2){
+            else if(majEle2 == nums[i]){
                 cnt2++;
             }
-            else if(cnt1 == 0 ){
-                maj1 = nums[i];
+            else if(cnt1 == 0){
+                majEle1 = nums[i];
                 cnt1 = 1;
             }
-            else if(cnt2 == 0 ){
-                maj2 = nums[i];
+            else if(cnt2 == 0){
+                majEle2 = nums[i];
                 cnt2 = 1;
             }
-            else{
+            else {
                 cnt1--;
                 cnt2--;
             }
         }
 
-        vector<int>res ;
         int f1 = 0;
         int f2 = 0;
-        for(int &it : nums){
-            if(it == maj1){
+        for(int &num : nums){
+            if(num == majEle1){
                 f1++;
             }
-            else if(it == maj2){
+            else if(num == majEle2){
                 f2++;
             }
         }
+        vector<int>arr;
+        if(f1 > n/3) arr.push_back(majEle1);
+        if(f2 > n/3) arr.push_back(majEle2);
 
-        if(f1 > floor(n/3)) res.push_back(maj1);
-        if(f2 > floor(n/3)) res.push_back(maj2);
-
-        return res;
+        return arr;
     } 
 };
