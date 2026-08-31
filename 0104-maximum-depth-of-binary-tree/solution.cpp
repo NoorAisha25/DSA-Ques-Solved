aa78b0ba4897443434100852f68322bfb23bde41
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-31 18:10:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22 MB (87.28%)

--- 0104-maximum-depth-of-binary-tree/solution.cpp
+++ 0104-maximum-depth-of-binary-tree/solution.cpp
@@ -13,8 +13,8 @@ class Solution {
 public:
     int maxDepth(TreeNode* root) {
         if(root == NULL) return 0;
-        int leftDepth = maxDepth( root -> left);
-        int rightDepth = maxDepth( root -> right);
-        return 1 + max(leftDepth , rightDepth);
+        int left = maxDepth(root -> left);
+        int right = maxDepth(root -> right);
+        return 1 + max(left , right);
     }
 };
