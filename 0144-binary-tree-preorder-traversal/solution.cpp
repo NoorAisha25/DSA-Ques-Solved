acc3d1d5aed276db5eab1f518dfe4f8880c361e9
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-31 17:58:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11 MB (44.34%)

--- 0144-binary-tree-preorder-traversal/solution.cpp
+++ 0144-binary-tree-preorder-traversal/solution.cpp
@@ -11,22 +11,15 @@
  */
 class Solution {
 public:
+    void preorder(TreeNode* root , vector<int>&arr){
+        if(root == NULL) return ;
+        arr.push_back(root->val);
+        preorder(root->left , arr);
+        preorder(root->right , arr);
+    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>arr;
-        if(root == NULL ) return arr;
-
-        stack<TreeNode*>st;
-        st.push(root);
-
-        while(!st.empty()){
-            root = st.top();
-            st.pop();
-
-            arr.push_back(root->val);
-            if(root->right) st.push(root->right);
-            if(root->left) st.push(root->left);
-            
-        }
-        return arr;
+       vector<int>Arr;
+       preorder(root , Arr);
+       return Arr;
     }
 };
