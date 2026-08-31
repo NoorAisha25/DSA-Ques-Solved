e74b807d8b533da5b8a643419dd16068c16fc609
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-31 16:05:42 +0000

    Sync LeetCode submission Runtime - 15 ms (92.25%), Memory - 17.4 MB (87.48%)

--- 0018-4sum/solution.cpp
+++ 0018-4sum/solution.cpp
@@ -3,51 +3,44 @@ public:
     
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
          int n = nums.size();
-
-         vector<vector<int>> result;
+         vector<vector<int>>res;
 
          if(n < 4) return {};
-
          sort(nums.begin() , nums.end());
-
-         for(int i = 0 ; i< n-3 ; i++){
-
-            if(i>0 && nums[i]==nums[i-1]) continue;
+         
+         for(int i = 0 ; i < n-3 ; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
             int n1 = nums[i];
+            for(int j = i+1 ; j < n -2 ; j ++ ){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
-               for(int j = i+1 ; j< n-2 ; j++){
-                 
-                 if(j>i+1 && nums[j] == nums[j-1]) continue;
-                   
-                   int n2 = nums[j];
-            
-                 long long newtarget = (long long)target - n1 - n2;
+                int n2 = nums[j];
 
-                    int sum = 0;
-         int l = j + 1;
-         int m = n-1;
+                long long newT = (long long)target - n1 - n2;
 
-         while(l < m){
-            sum = nums[l] + nums[m];
+                int sum = 0;
+                int l = j + 1;
+                int r = n - 1;
 
-            if(sum > newtarget) m--;
-            else if ( sum < newtarget) l++;
+                while(l < r){
+                    sum = nums[l] + nums[r];
 
-            else{
-                while( l<m && nums[l] == nums[l+1]) l++;
-                while(l<m && nums[m] == nums[m-1]) m--;
+                    if(sum < newT) l++;
+                    else if(sum > newT) r--;
 
-                result.push_back ({nums[i] , nums[j] , nums[l] , nums[m]});
+                    else {
+                        while(l < r && nums[l] == nums[l+1]) l++;
+                        while(l < r && nums[r] == nums[r-1]) r--;
 
-                l++;
-                m--;
+                        res.push_back({nums[i] , nums[j] , nums[l] , nums[r]});
+
+                        l++;
+                        r--;
+                    }
+                }
             }
          }
-      }
-    }
-        
-         return result;
-
+         return res;
     }
 };
