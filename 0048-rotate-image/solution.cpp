7ad39b9806f77dd7574419bfde3cc82aa486817b
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-31 14:31:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.2 MB (35.48%)

--- 0048-rotate-image/solution.cpp
+++ 0048-rotate-image/solution.cpp
@@ -2,23 +2,14 @@ class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        // vector<vector<int>> ans(n , vector<int>(n));
-
-        // for(int i = 0 ; i<n ; i++){
-        //     for(int j = 0 ; j < n ; j++){
-        //         ans[j][n-1-i] = matrix[i][j];
-        //     }
-        // }
-        // matrix = ans;
-        
-    
-    // this is optimal approach of sp com ---O(1) --- bcz no space taken
-        for(int i = 0 ; i<n ; i++){
-            for(int j = i ; j<n ; j++){
+        for(int i = 0 ; i < n ; i++){
+            for(int j = i ; j < n ; j++){
                 swap(matrix[i][j] , matrix[j][i]);
             }
         }
-        for(int i = 0 ; i<n ; i++){
-        reverse(matrix[i].begin() , matrix[i].end());
-    }}
+
+        for(int i = 0 ; i < n ; i++){
+            reverse(matrix[i].begin() , matrix[i].end());
+        }
+    }
 };
