8df3d5aaa02e2c8177859d03bbf0a96629c946a3
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-08-31 18:13:22 +0000

    Sync LeetCode submission Runtime - 3 ms (9.33%), Memory - 23 MB (83.06%)

--- 0110-balanced-binary-tree/solution.cpp
+++ 0110-balanced-binary-tree/solution.cpp
@@ -14,16 +14,17 @@ public:
     bool isBalanced(TreeNode* root) {
         return height(root) != -1;
     }
-
     int height(TreeNode* root){
-        if(root == NULL ) return 0;
+        if(root == NULL) return 0;
 
-        int leftDepth = height(root -> left);
-        if(leftDepth == -1) return -1;
-        int rightDepth = height(root -> right);
-        if(rightDepth == -1) return -1;
+        int left = height(root -> left);
+        if(left == -1) return -1;
 
-        if(abs(leftDepth - rightDepth) > 1) return -1;
-        return 1 + max(leftDepth , rightDepth);
+        int right = height(root -> right);
+        if(right == -1) return -1;
+
+        if(abs(left - right) > 1) return -1;
+
+        return 1 + max(left , right);
     }
 };
