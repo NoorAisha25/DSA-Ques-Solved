e00b4803da25ee62ca85c73a5bc43c6b243f7c6d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-01 18:26:38 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (13.90%)

--- 0069-sqrtx/solution.cpp
+++ 0069-sqrtx/solution.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int mySqrt(int x) {
         int l = 1;
-        int h = x ;
-        if( x == 0) return 0;
+        int h = x;
+
+        if(x == 0) return 0;
         int res = 0;
         while(l <= h){
-           long long mid = l + ( h - l)/2;
+            long long m = l + (h - l)/2;
 
-           if( mid * mid <= x){
-            res = mid;
-            l = mid +  1;
-           }
-           else h = mid - 1;
+            if(m * m <= x){
+                res = m;
+                l = m + 1;
+            }
+            else h = m  - 1;
         }
         return res;
     }
