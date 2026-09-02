8b883c1e7a6f26d6b4b3f426e2f807e47ff4f3db
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-02 17:39:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.8 MB (80.81%)

--- 1078-remove-outermost-parentheses/solution.cpp
+++ 1078-remove-outermost-parentheses/solution.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+       string res ="";
        int depth = 0;
-       string res= "";
 
-       for(char &c : s){
-
-            if(c == '('){
-                if(depth >0) res.push_back(c);
-                depth++;
-            }
-            else{
-                depth--;
-                if(depth > 0) res.push_back(c);
-            }
+       for(char c : s){
+          if(c == '('){
+             if(depth > 0) res.push_back(c);
+             depth++;
+          }
+          else{
+            depth--;
+            if(depth > 0) res.push_back(c);
+          }
        }
        return res;
     }
