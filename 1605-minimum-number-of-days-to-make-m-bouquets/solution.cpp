ed6b4e63598b5b7846b423b5ecbc4b358b84a526
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-03 18:34:53 +0000

    Sync LeetCode submission Runtime - 17 ms (87.13%), Memory - 70.4 MB (98.74%)

--- 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
-    int fun(vector<int>& arr, int m, int k){
+    int solve(vector<int>&arr , int mid , int k){
         int boq = 0;
         int curr = 0;
 
-        for(int i = 0 ; i<arr.size() ; i++){
-
-            if(arr[i] <= m){
+        for(int i = 0 ; i < arr.size() ; i++){
+              if(arr[i] <= mid){
                 curr++;
-            }
-            else { curr = 0;}
-            if(curr == k){
+              }
+              else{
+                curr = 0;
+              }
+              if(curr == k){
                 boq++;
-                curr=0;
-            }
+                curr = 0;
+              }
         }
         return boq;
     }
     int minDays(vector<int>& arr, int m, int k) {
+        int n = arr.size();
         int l = 1;
         int h = *max_element(arr.begin() , arr.end());
         int mini = -1;
-        while( l <= h){
-            int mid = l + ( h - l )/2;
-            int possAns = fun(arr , mid , k);
 
-            if( possAns >= m){
+        while(l <= h){
+            int mid = l + (h  -  l)/2;
+
+            int pos = solve(arr , mid , k);
+            if(pos >= m){
                 mini = mid;
                 h = mid - 1;
             }
-            else l = mid + 1;
+            else{
+                l = mid + 1 ;
+            }
         }
         return mini;
     }
