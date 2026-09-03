52b87f281753868ae840d4e6fde8f9305f1ce0f4
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-03 19:23:34 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.4 MB (73.35%)

--- 0410-split-array-largest-sum/solution.cpp
+++ 0410-split-array-largest-sum/solution.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-    int pos(vector<int>& nums, int mid){
+    int fun(vector<int>&arr , int m , int k){
+        int sum = 0;
+        int cnt = 1;
+        for(int i = 0 ; i < arr.size() ; i++){
+            sum += arr[i];
 
-        int subarrays = 1 ;
-        int cumSum = 0;
-
-        for(int i = 0 ; i<nums.size() ; i++){
-            cumSum += nums[i];
-
-            if(cumSum > mid){
-                subarrays++;
-                cumSum = nums[i];
+            if(sum > m){
+                cnt++;
+                sum = arr[i];
             }
         }
-        return subarrays;
+        return cnt;
     }
-    int func(vector<int>& nums, int k){
+    int solve( vector<int>& arr , int k){
+        int n = arr.size();
+        int l = *max_element(arr.begin() , arr.end());
+        int h = accumulate(arr.begin() , arr.end() , 0);
+        int mini = 0;
 
-        int n = nums.size();
-        int l = *max_element(nums.begin() , nums.end());
-        int h = accumulate(nums.begin() , nums.end(), 0);
-        int ans = -1;
-        while( l <= h){
-            
-            int mid = l + ( h - l)/2;
-            int possAns = pos(nums , mid);
+        while(l <= h){
+            int m = l + (h - l)/2;
+            int pos = fun(arr , m , k);
 
-            if(possAns <= k){
-                ans = mid;
-                h = mid - 1;
+            if(pos <= k){
+                mini = m;
+                h = m - 1;
             }
-            else l = mid + 1;
+            else l = m  + 1;
         }
-        return ans;
-
+        return mini;
     }
     int splitArray(vector<int>& nums, int k) {
-        return func(nums, k);
+        return solve(nums , k);
     }
 };
