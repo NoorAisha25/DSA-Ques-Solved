0aedafb13ec107ea6011d32c6f4e95f364e12731
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 17:23:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.8 MB (23.35%)

--- 0198-house-robber/solution.cpp
+++ 0198-house-robber/solution.cpp
@@ -8,9 +8,8 @@ public:
         vector<int>dp(n);
         dp[0] = nums[0];
         dp[1] = max(nums[0] , nums[1]);
-
         for(int i = 2 ; i < n ; i++){
-            dp[i] = max( dp[i-1] , dp[i-2] + nums[i]);
+            dp[i] = max(dp[i-1] , dp[i-2] + nums[i]);
         }
         return dp[n-1];
     }
