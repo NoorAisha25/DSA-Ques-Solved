992a08f7510c6a88554891e5b242e95baf057215
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 17:39:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.3 MB (40.95%)

--- 0064-minimum-path-sum/solution.cpp
+++ 0064-minimum-path-sum/solution.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
+        int m = grid.size();
+        int n = grid[0].size();
 
-        vector<vector<int>>t(n , vector<int>(m , 0));
-
+        vector<vector<int>>t(m , vector<int>(n , 0));
+        
         t[0][0] = grid[0][0];
 
-        for(int col = 1 ; col < m ; col++){
+        for(int col = 1 ; col < n ; col++){
             t[0][col] = grid[0][col] + t[0][col-1];
         }
 
-        for(int row = 1 ; row < n ; row++){
+        for(int row = 1 ; row < m ; row++){
             t[row][0] = grid[row][0] + t[row-1][0];
         }
 
-        for(int i = 1 ; i  < n ; i++){
-            for(int j = 1 ; j < m ; j++){
+        for(int i = 1 ; i < m ; i++){
+            for(int j = 1 ; j < n ; j++){
                 t[i][j] = grid[i][j] + min(t[i-1][j] , t[i][j-1]);
             }
         }
 
-        return t[n-1][m-1];
+        return t[m-1][n-1];
     }
 };
