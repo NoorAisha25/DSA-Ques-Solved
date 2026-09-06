53ed1b8854e642d4f28bf0e22b730001b22d125a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 15:51:44 +0000

    Sync LeetCode submission Runtime - 4 ms (21.62%), Memory - 14.2 MB (28.69%)

--- 0040-combination-sum-ii/solution.cpp
+++ 0040-combination-sum-ii/solution.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    void solve(int i ,vector<int>& arr, vector<vector<int>> &res ,vector<int>&comb , int target ){
-        
-        if(i > arr.size() || target < 0) return;
-        
-        if(target == 0){
+    void solve(int i , vector<int>& arr, int k , vector<vector<int>>& res, vector<int>& comb){
+
+        if(i > arr.size() || k < 0) return;
+
+        if(k == 0){
             res.push_back(comb);
             return;
         }
-        
+
         for(int j = i ; j < arr.size() ; j++){
-            if( j > i && arr[j] == arr[j-1]) continue;
+            if(j > i && arr[j] == arr[j-1]) continue;
 
             comb.push_back(arr[j]);
-            solve(j+1 , arr , res , comb , target-arr[j]);
+            solve(j + 1, arr, k - arr[j] , res, comb);
             comb.pop_back();
         }
     }
     vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
-        sort(arr.begin() , arr.end());
-        vector<vector<int>> res;
+        vector<vector<int>>res;
         vector<int>comb;
+        sort(arr.begin() , arr.end());
 
-        solve(0 , arr , res , comb , target);
+        solve(0 , arr , target , res ,comb);
         return res;
     }
 };
