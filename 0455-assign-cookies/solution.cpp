cb64c64a48cbf4ead2c5461abbb3ef122b70f77d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 17:47:57 +0000

    Sync LeetCode submission Runtime - 12 ms (28.81%), Memory - 45 MB (11.86%)

--- 0455-assign-cookies/solution.cpp
+++ 0455-assign-cookies/solution.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        int m = g.size();
         int n = s.size();
+        int m = g.size();
+
+        int i = 0;
+        int j = 0;
 
         sort(s.begin() , s.end());
         sort(g.begin() , g.end());
 
-        int i = 0 ;
-        int j = 0 ;
-
-        while( i < n && j < m){
+        while(i < n && j < m){
             if(s[i] >= g[j]){
                 j++;
             }
