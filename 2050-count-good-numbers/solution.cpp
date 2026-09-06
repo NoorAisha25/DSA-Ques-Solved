0cb95ff9bfb6be17774e322d37ee2c58f6f5b543
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 14:47:27 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (37.36%)

--- 2050-count-good-numbers/solution.cpp
+++ 2050-count-good-numbers/solution.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int m = 1e9 + 7;
-    long long find( long long x , long long n){ // x^n
-
-        if( n == 0 ) return 1;
-        long long half = find( x , n/2);
+    long long solve(long long x , long long n){
+        if (n == 0) return 1;
+        long long half = solve(x , n/2);
         long long res = (half * half)%m;
-        if( n %2 == 1){
-            res = (res * x)%m;
+        if(n % 2 != 0){
+            res =  (x * res)%m;
         }
         return res;
     }
     int countGoodNumbers(long long n) {
-        return (long long) find( 5 , (n+1)/2) * find( 4 , n/2)%m;
+        return (long long) solve(5 , (n+1)/2) * solve(4 , n / 2)%m;
     }
 };
