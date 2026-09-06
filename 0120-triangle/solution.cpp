30dafa1c769b0edb16af1db67a64901efc98f537
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 17:42:31 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 12.5 MB (97.32%)

--- 0120-triangle/solution.cpp
+++ 0120-triangle/solution.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int minimumTotal(vector<vector<int>>& triangle) {
-        int m = triangle.size();
+    int minimumTotal(vector<vector<int>>& arr) {
+        int n = arr.size();
 
-        for(int i = m-2 ; i >= 0 ; i--){
-            for(int j = 0 ; j < triangle[i].size() ; j++){
-                triangle[i][j] += min(triangle[i+1][j] , triangle[i+1][j+1]);
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int j = 0 ; j < arr[i].size() ; j++){
+                arr[i][j] += min(arr[i+1][j] , arr[i+1][j+1]);
             }
         }
-        return triangle[0][0];
+        return arr[0][0];
     }
 };
