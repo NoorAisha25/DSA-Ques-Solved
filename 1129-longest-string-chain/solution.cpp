80aa53df0ba9f7ba8148c2ae4d4d79f474e6c473
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-06 18:41:11 +0000

    Sync LeetCode submission Runtime - 1883 ms (10.44%), Memory - 640.2 MB (20.61%)

--- 1129-longest-string-chain/solution.cpp
+++ 1129-longest-string-chain/solution.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
-    bool check(string& prev , string& curr){
-        if(prev.size() + 1 != curr.size()) return false;
+    bool check(string prev , string curr ){
+        if( prev.size()+ 1 != curr.size()) return false;
 
         int i = 0 ;
-        int j = 0 ;
+        int j = 0;
 
-        while(i < prev.size() && j < curr.size()){
+        while( i < prev.size() && j < curr.size()){
             if(prev[i] == curr[j]){
                 i++;
             }
             j++;
         }
+
         return i == prev.size();
     }
     int longestStrChain(vector<string>& words) {
         int n = words.size();
 
-        sort(words.begin() , words.end() , [](string&A , string&B){
-            return A.size() < B.size();
+        sort(words.begin() , words.end() , [](string& a , string& b){
+            return a.size() < b.size();
         });
 
         vector<int>dp(n , 1);
         int len = 1;
-
-        for(int i = 1 ; i < n ; i++ ){
+        for(int i = 1 ; i < n ; i++){
             for(int j = 0 ; j < i ; j++){
                 if(check(words[j] , words[i])){
                     dp[i] = max(dp[i] , dp[j] + 1);
                 }
             }
-            len = max( len , dp[i]);
+            len = max(len , dp[i]);
         }
         return len;
     }
