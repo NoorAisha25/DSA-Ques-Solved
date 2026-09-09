ac7ce5852ca165a0e7b74d660edbd06d630762c4
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-09 13:20:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (14.57%)

---
+++ 4245-count-commas-in-range/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countCommas(int n) {
+       long long ans = 0;
+
+       if(n >= 1000) ans += (n - 999);
+       if(n >= 1000000) ans += n - 999999;
+       return ans;
+    }
+};
