1bf5cd660a89e097855aea928f5c269f61178e20
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-09 10:28:05 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 30.3 MB (30.32%)

---
+++ 4256-construct-uniform-parity-array-i/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int n = nums1.size();
+        if(n <= 1) return true;
+
+        bool hasEven = false;
+        bool hasOdd = false;
+        for(int i = 0 ; i < n  ; i++){
+            if(nums1[i]%2 == 0) hasEven = true;
+            else hasOdd = true;
+        }
+        return true;
+    }
+};
