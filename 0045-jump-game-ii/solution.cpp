7f27f81e0acbf83309fe702c0783a4a13fe1f402
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-10 08:01:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 20.7 MB (29.03%)

--- 0045-jump-game-ii/solution.cpp
+++ 0045-jump-game-ii/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
+        //two pointer approach
         int n = nums.size();
-
-        int minjump = 0 ; //minimum jump to reach n-1;
+        int miniJump = 0;
         int l = 0 , r = 0;
-
-        while( r < n-1){
-               int maxJump = 0;
-               for(int i = l ; i <= r ; i++){
-                  maxJump = max( nums[i] + i , maxJump);
-               }
-               minjump++;
-               l = r + 1;
-               r = maxJump;
+        while(r < n-1){
+            int maxiJump = 0;
+            for(int i = l ; i <= r ; i++){
+                maxiJump = max(nums[i] + i , maxiJump);
+            }
+            miniJump++;
+            l = r + 1;
+            r = maxiJump;
         }
-        return minjump;
+        return miniJump;
     }
 };
