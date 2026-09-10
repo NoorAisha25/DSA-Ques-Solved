e66d7ef39479f514d648c6ddd15b1921198d4666
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-10 08:22:46 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 30.4 MB (67.42%)

---
+++ 1428-jump-game-iii/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        int n = arr.size();
+        vector<bool>visited(n , false);
+        return dfs(arr , start , visited);
+    }
+
+    bool dfs(vector<int>& arr , int i , vector<bool>& visited){
+        if(i < 0 || i >= arr.size() || visited[i]) return false;
+
+        if(arr[i] == 0) return true;
+        visited[i] = true;
+        return dfs(arr , i + arr[i] , visited) || dfs(arr , i - arr[i] , visited);
+    }
+};
