3c98a6870ad19fb95e35e89900d48041b5fdc951
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-10 07:15:11 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 87.2 MB (44.33%)

--- 0890-lemonade-change/solution.cpp
+++ 0890-lemonade-change/solution.cpp
@@ -6,26 +6,27 @@ public:
        int ten = 0;
 
        for(int bill : bills){
-
-           if(bill == 5) five++;
-           else if(bill == 10){
-              if(five > 0){
-                 five--;
-                 ten++;
-              }
-              else return false;
-           }
-           else if(bill == 20){
-               if(five > 0 && ten > 0){
-                   five--;
-                   ten--;
-               }
-               else if( five >= 3){
-                   five -= 3;
-               }
-               else return false;
-           }
+         if(bill == 5){
+            five++;
+         }
+         else if(bill == 10){
+            if(five > 0){
+                five--;
+                ten++;
+            }
+            else return false;
+         }
+         else {
+            if(five > 0 && ten > 0){
+                five--;
+                ten--;
+            }
+            else if(five >= 3){
+                five -= 3;
+            }
+            else return false;
+         }
        }
-       return true; 
+       return true;
     }
 };
