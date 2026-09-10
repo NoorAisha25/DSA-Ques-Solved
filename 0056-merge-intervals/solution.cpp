b5a559a4918a8df73e228b5e872910dfae1fd45e
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-10 10:28:47 +0000

    Sync LeetCode submission Runtime - 9 ms (16.96%), Memory - 23.7 MB (95.10%)

--- 0056-merge-intervals/solution.cpp
+++ 0056-merge-intervals/solution.cpp
@@ -1,21 +1,19 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& arr) {
+         vector<vector<int>>res;
          int n = arr.size();
-         vector<vector<int>>ans;
          sort(arr.begin() , arr.end());
-         ans.push_back(arr[0]);
-         int i = 1;
-         
-         while(i < arr.size()){
-            if(arr[i][0] <= ans.back()[1]){
-                ans.back()[1] = max(arr[i][1] , ans.back()[1]);
+         res.push_back(arr[0]);
+
+         for(int i = 1 ; i < n ; i++){
+            if(arr[i][0] <= res.back()[1]){
+                res.back()[1] = max(arr[i][1] , res.back()[1]);
             }
             else{
-                ans.push_back(arr[i]);
+                res.push_back(arr[i]);
             }
-            i++;
          }
-         return ans;
+         return res;
     }
 };
