fdf3d7ded9af444e158a0367eeaefcdb71b84725
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-11 19:12:45 +0000

    Sync LeetCode submission Runtime - 81 ms (33.12%), Memory - 193.4 MB (54.18%)

--- 0943-sum-of-subarray-minimums/solution.cpp
+++ 0943-sum-of-subarray-minimums/solution.cpp
@@ -1,43 +1,48 @@
 class Solution {
 public:
-    vector<int> PSE(vector<int>& arr){
-      int n = arr.size();
-      vector<int>ans(n , -1);
-      stack<int>st;
+    vector<int> NSE(vector<int>&arr){
+        int n = arr.size();
+        stack<int>st;
+        vector<int>ans(n , n);
 
-      for(int i = 0 ; i< n ; i++){
-         while(!st.empty() && arr[st.top()] > arr[i]) st.pop();
-         if(!st.empty()){
-             ans[i] = st.top();
-         }
-         st.push(i);
-      }
-      return ans;
+        for(int i = n - 1; i >= 0 ; i--){
+            while(!st.empty() && arr[st.top()] >= arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
     }
 
-    vector<int> NSE(vector<int>& arr){
-      int n = arr.size();
-      vector<int>ans(n , n);
-      stack<int>st;
+     vector<int> PSE(vector<int>&arr){
+        int n = arr.size();
+        stack<int>st;
+        vector<int>ans(n , -1);
 
-      for(int i = n-1 ; i >= 0 ; i--){
-         while(!st.empty() && arr[st.top()] >= arr[i]) st.pop();
-         if(!st.empty()){
-             ans[i] = st.top();
-         }
-         st.push(i);
-      }
-      return ans;
+        for(int i = 0 ; i < n ; i++){
+            while(!st.empty() && arr[st.top()] > arr[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                ans[i] = st.top();
+            }
+            st.push(i);
+        }
+        return ans;
     }
+
     int sumSubarrayMins(vector<int>& arr) {
-       vector<int>pse = PSE(arr);
        vector<int>nse = NSE(arr);
+       vector<int>pse = PSE(arr);
+       int n = arr.size();
 
-       int m = 1e9+7 ;
        long long sum = 0;
-       
-       int i = 0;
-       while( i < arr.size()){
+       int m = 1e9 + 7;
+
+       for(int i = 0 ; i < n ; i++){
           long long left = i - pse[i];
           long long right = nse[i] - i;
 
@@ -45,8 +50,7 @@ public:
 
           long long totalSum = arr[i] * totalways;
 
-          sum = (sum + totalSum)%m;
-          i++;
+          sum = (sum + totalSum )%m;
        }
        return sum;
     }
