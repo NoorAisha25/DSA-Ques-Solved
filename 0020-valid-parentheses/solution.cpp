a94b73d4732cf58b4595e1e83799149a5d8ae99a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-11 15:14:55 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (36.93%)

--- 0020-valid-parentheses/solution.cpp
+++ 0020-valid-parentheses/solution.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int>st;
+        stack<char>st;
 
         for(char c : s){
-            if(c == '(' || c == '{' || c == '['){
-                st.push(c);
-            }
+            if(c == '(' || c == '{' || c == '[') st.push(c);
             else{
                 if(st.empty()) return false;
                 char ch = st.top();
                 st.pop();
-                if((c == ')' && ch != '(') || (c == '}' && ch != '{') || (c == ']' && ch != '[')){
-                    return false;
-                }
+
+                if( (c == ')' && ch != '(') || (c == '}' && ch != '{') || (c == ']' && ch != '[')) return false;
             }
         }
         return st.empty();
