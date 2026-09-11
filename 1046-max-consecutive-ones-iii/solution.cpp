f031204f4a723690a1eadaf2eff31ff425316010
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-11 10:52:35 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 69.6 MB (70.38%)

--- 1046-max-consecutive-ones-iii/solution.cpp
+++ 1046-max-consecutive-ones-iii/solution.cpp
@@ -4,11 +4,15 @@ public:
         int n = nums.size();
         int l = 0;
         int r = 0;
+        int maxOne = 0;
         int zero = 0;
-        int maxLen = 0;
 
-        while( r < n){
+        while(r < n){
             if(nums[r] == 0) zero++;
+
+            if(zero <= k){
+                maxOne = max( maxOne , r - l + 1);
+            }
 
             if(zero > k){
                 if(nums[l] == 0){
@@ -16,12 +20,8 @@ public:
                 }
                 l++;
             }
-
-            if(zero <= k){
-                maxLen = max( maxLen , r - l  + 1);
-            }
             r++;
         }
-        return maxLen;
+        return maxOne;
     }
 };
