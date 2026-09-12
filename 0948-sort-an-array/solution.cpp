76b65143aba93c39b68e15060e29a5f219e43ec5
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-12 08:25:14 +0000

    Sync LeetCode submission Runtime - 2791 ms (5.03%), Memory - 74.1 MB (70.36%)

--- 0948-sort-an-array/solution.cpp
+++ 0948-sort-an-array/solution.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
-    int partition(vector<int>& nums,int low,int high){
-        int pivot= nums[low];
-        int i=low;
-        int j=high;
-        while(i<j){
-            while((nums[i]<=pivot) && i<=high-1){
-                i++;
-            }
-            while((nums[j]>pivot) && j>=low+1){
-                j--;
-            }
-            if(i<j){
-                swap(nums[i],nums[j]);
-            }
+    int partition(vector<int>&arr , int l , int h){
+        int pi = arr[l];
+        int i = l;
+        int j = h;
+
+        while(i < j ){
+            while(arr[i] <= pi && i <= h - 1) i++;
+            while(arr[j] > pi && j > l) j--;
+            if(i < j) swap(arr[i] , arr[j]);
         }
-        swap(nums[low],nums[j]);
+        swap(arr[j] , arr[l]);
         return j;
     }
-    void quickSort(vector<int>& nums,int low, int high) {
-        if(low<high){
-           int PIele = partition(nums,low,high);
-            quickSort(nums,low,PIele-1);
-            quickSort(nums,PIele+1,high);
+    void qs(vector<int>& arr , int l , int h){
+
+        if(l < h){
+            int pi = partition(arr , l , h);
+            qs(arr , l , pi - 1);
+            qs(arr, pi + 1 , h);
         }
-    }    
-     vector<int> sortArray(vector<int>& nums) {
-        quickSort(nums, 0, nums.size() - 1);
-        return nums;
+    }
+    vector<int> sortArray(vector<int>& nums) {
+       qs(nums , 0 , nums.size() - 1);
+       return nums;
     }
     
 };  
