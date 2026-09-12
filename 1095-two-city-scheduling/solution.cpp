0607ae83d8a9c8e171eb6fbac8992fdc8ecc234a
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-12 12:42:00 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.9 MB (95.20%)

---
+++ 1095-two-city-scheduling/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int twoCitySchedCost(vector<vector<int>>& costs) {
+        sort(costs.begin() , costs.end() , [](const vector<int>&a ,const vector<int>&b){
+            return (a[1] - a[0]) < (b[1] - b[0]);
+        });
+
+        int n = costs.size();
+        long long ans = 0;
+
+        for(int i = 0 ; i < n ; i++){
+            if(i < n / 2){
+                ans += costs[i][1];
+            }
+            else{
+                ans += costs[i][0];
+            }
+        }
+
+        return ans;
+    }
+};
