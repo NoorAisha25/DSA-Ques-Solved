bc4904976b9b2015658bd9a54f814b8451a50a8b
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-12 12:46:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.7 MB (86.73%)

--- 0152-maximum-product-subarray/solution.cpp
+++ 0152-maximum-product-subarray/solution.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
-        
+
+        int currmax = nums[0];
         int currmin = nums[0];
-        int currmax= nums[0];
         int maxi = nums[0];
 
         for(int i = 1 ; i < n ; i++){
-            int temp = max({nums[i] , nums[i] * currmin , nums[i] * currmax});
-            currmin = min({nums[i] , nums[i] * currmin , nums[i] * currmax});
+            int temp = max({ nums[i] , currmax*nums[i] , currmin * nums[i]});
+            currmin = min({nums[i] , currmin*nums[i] , currmax * nums[i]});
             currmax = temp;
             maxi = max( maxi , currmax);
         }
