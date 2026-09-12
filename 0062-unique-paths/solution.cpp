99ee2d42459c6b37ebf7ea88f07b210b464ef73e
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-12 14:39:53 +0000

    Sync LeetCode submission Runtime - 3 ms (9.59%), Memory - 9.3 MB (53.29%)

--- 0062-unique-paths/solution.cpp
+++ 0062-unique-paths/solution.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>t(m , vector<int>(n , -1));
+        vector<vector<int>> t( m , vector<int>(n , -1));
 
         t[0][0] = 1;
 
@@ -14,9 +14,9 @@ public:
         }
 
         for(int i = 1 ; i < m ; i++){
-            for(int j = 1 ; j < n ; j++){
-                t[i][j] = t[i-1][j] + t[i][j-1];
-            }
+          for(int j = 1 ; j  < n ; j++){
+             t[i][j] = t[i-1][j] + t[i][j-1];
+          }
         }
 
         return t[m-1][n-1];
