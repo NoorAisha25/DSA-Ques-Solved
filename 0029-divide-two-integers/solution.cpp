a0ae3489bd6394dc910267e84f0785f1b3f63ed9
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-13 14:41:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.7 MB (39.44%)

--- 0029-divide-two-integers/solution.cpp
+++ 0029-divide-two-integers/solution.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         
-        if( dividend == INT_MIN && divisor == -1) return INT_MAX;
+      if(dividend == INT_MIN && divisor == -1) return INT_MAX;
 
-        bool negative = ( divisor < 0) ^ (dividend < 0);
-        long long a = abs((long long)(dividend));
-        long long b = abs((long long )(divisor));
+      int neg = (dividend < 0) ^ (divisor < 0);
+      long long a = abs((long long)(dividend));
+      long long b = abs((long long)(divisor));
 
-        long long res = 0;
+      long long res = 0;
 
-        for(int i = 31 ; i >=0 ; i--){
-            if((b << i) <= a){
-                a -= (b << i);
-                res += (1 << i);
-            }
+      for(int i = 31 ; i >= 0 ; i--){
+        if((b << i) <= a){
+            a -= (b << i);
+            res += (1 << i);
         }
-        return negative ? (int)-res : (int)res;
+      }
+
+      return neg ? (int)-res : (int)res;
     }
 };
