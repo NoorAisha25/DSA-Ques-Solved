7678b687da7940c208f8a38f2f19e6cae3abb9ad
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-13 16:26:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (13.90%)

--- 0069-sqrtx/solution.cpp
+++ 0069-sqrtx/solution.cpp
@@ -10,10 +10,10 @@ public:
             long long m = l + (h - l)/2;
 
             if(m * m <= x){
-                res = m;
-                l = m + 1;
+                 res = m;
+                 l = m + 1;
             }
-            else h = m  - 1;
+            else h = m - 1;
         }
         return res;
     }
