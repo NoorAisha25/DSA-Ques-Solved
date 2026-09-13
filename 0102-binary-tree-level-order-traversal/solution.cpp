570092e0dd9ea5060b7ef811a9ccc465e7442109
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-13 10:56:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 17.3 MB (15.55%)

--- 0102-binary-tree-level-order-traversal/solution.cpp
+++ 0102-binary-tree-level-order-traversal/solution.cpp
@@ -17,14 +17,14 @@ public:
 
        queue<TreeNode*>q;
        q.push(root);
+
        while(!q.empty()){
-          int n = q.size();
           vector<int>level;
+          int n = q.size();
           for(int i = 0 ; i < n ; i++){
              TreeNode* curr = q.front();
              q.pop();
              level.push_back(curr->val);
-
              if(curr->left) q.push(curr->left);
              if(curr->right) q.push(curr->right);
           }
