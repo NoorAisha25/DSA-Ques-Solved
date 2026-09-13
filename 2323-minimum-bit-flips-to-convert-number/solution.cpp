27d4c9ae91113e327d82b1d08bddd610ab58664e
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-13 14:49:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (92.37%)

--- 2323-minimum-bit-flips-to-convert-number/solution.cpp
+++ 2323-minimum-bit-flips-to-convert-number/solution.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int minBitFlips(int start, int goal) {
-        int ans = start ^ goal;
-        int cnt = 0;
+       int cnt = 0;
+       int ans = start ^ goal;
 
-        for(int i = 0 ; i < 32 ; i++){
-            if(ans & (1 << i)) cnt++;
-        }
-        return cnt;
+       for(int i = 31 ; i >= 0 ; i--){
+          if(ans & (1 << i)) cnt++;
+       }
+       return cnt;
     }
 };
