e722c08cc1eb80630d2e397cea2298dd9063b0c3
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-13 10:35:22 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 34.3 MB (50.76%)

--- 0450-delete-node-in-a-bst/solution.cpp
+++ 0450-delete-node-in-a-bst/solution.cpp
@@ -12,23 +12,23 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root == NULL) return NULL;
-        if(key < root -> val) root -> left = deleteNode(root->left , key);
-        else if( key > root -> val) root -> right = deleteNode(root -> right , key);
-        else{
-            if(!root -> left ) return root -> right;
-            if(!root -> right ) return root -> left;
+       if(root == NULL) return NULL;
+       if(key < root->val) root ->left = deleteNode(root->left , key);
+       else if(key > root->val) root->right = deleteNode(root->right , key);
+       else{
+           if(!root->left) return root->right;
+           if(!root->right) return root->left;
 
-            TreeNode* pre = findMax(root->left);
-            root -> val = pre -> val;
-            root -> left = deleteNode(root -> left , pre->val); 
-        }
-        return root;
+           TreeNode* pre = findMax(root ->left);
+           root->val = pre->val;
+           root -> left = deleteNode(root->left , pre->val);
+       }
+       return root;
     }
 
     TreeNode* findMax(TreeNode* root){
-        while(root -> right){
-            root = root -> right;
+        while(root->right){
+            root = root->right;
         }
         return root;
     }
