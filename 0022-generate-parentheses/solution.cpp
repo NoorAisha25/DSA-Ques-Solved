4ff54fd3184a9a61e3a14fe023b123b312ba2767
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-14 16:29:13 +0000

    Sync LeetCode submission Runtime - 113 ms (5.09%), Memory - 161.9 MB (5.00%)

--- 0022-generate-parentheses/solution.cpp
+++ 0022-generate-parentheses/solution.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
-    vector<string>res;
-
     bool isvalid(string &s){
         stack<char>st;
 
-        for(char &c : s){
+        for(char c : s){
             if(c == '(') st.push(c);
             else{
                 if(st.empty()) return false;
@@ -16,26 +14,27 @@ public:
         }
         return st.empty();
     }
-    void solve( string& ans ,int n){
+    void solve(string& s , int n ,  vector<string>&res){
 
-        if( ans.size() == 2 * n){
-            if(isvalid(ans)){
-                res.push_back(ans);
+        if(s.size() == 2 * n){
+            if(isvalid(s)){
+                res.push_back(s);
             }
             return;
         }
 
-        ans.push_back('(');
-        solve(ans , n);
-        ans.pop_back();
+        s.push_back('(');
+        solve(s , n , res);
+        s.pop_back();
 
-        ans.push_back(')');
-        solve(ans , n);
-        ans.pop_back();
+        s.push_back(')');
+        solve(s , n , res);
+        s.pop_back();
     }
     vector<string> generateParenthesis(int n) {
-       string ans = "";
-       solve(ans , n);
+       string s = "";
+       vector<string>res;
+       solve(s , n , res);
        return res;
     }
 };
