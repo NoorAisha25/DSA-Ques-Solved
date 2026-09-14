de3967b9ee04cf27d55571d8f3bd764c4fdd608b
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-14 15:35:50 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (53.47%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,22 +1,17 @@
 class Solution {
 public:
-    int solve(string& s, int i , long res , int sign ){
+    int solve(int i , long res , string &s , int sign){
+         if(i >= s.size() || !isdigit(s[i])){
+            return (int)(sign * res);
+         }
+           int digit = s[i] - '0';
 
-        if( i >= s.size() || !isdigit(s[i])){
-           return (int)(sign * res);
-        }
+           if(res > INT_MAX/10 || (res == INT_MAX/10 && digit>7)){
+             return (sign == 1) ? INT_MAX : INT_MIN;
+           }
 
-        
-            int digit = s[i] - '0';
-
-            if( res > INT_MAX/10 || (res == INT_MAX/10 && digit > 7)){
-                return (sign == 1) ? INT_MAX : INT_MIN;
-            }
-
-            res = res * 10 + digit;
-            i++;
-            
-            return solve( s , i , res , sign);    
+           res = res * 10 + digit;
+           return solve(i + 1 , res, s , sign);
     }
     int myAtoi(string s) {
         int n = s.size();
@@ -24,12 +19,13 @@ public:
         long res = 0;
         int i = 0;
 
-        while( i < n && s[i] == ' ') i++;
+        while(i < n && s[i] == ' ') i++;
 
-        if( i <n && ( s[i] == '+' || s[i] == '-')){
+        if(i < n && (s[i] == '+' || s[i] == '-')){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
-        }
-        return solve(s , i , 0 , sign);        
+        }       
+
+        return solve(i , 0 , s , sign);
     }
 };
