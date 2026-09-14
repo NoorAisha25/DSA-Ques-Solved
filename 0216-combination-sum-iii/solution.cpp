ce7d9d4c2516456d2ae5b024f02c5e52ec63ffd7
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-14 16:56:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (82.11%)

--- 0216-combination-sum-iii/solution.cpp
+++ 0216-combination-sum-iii/solution.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    void solve(int i ,  int n , vector<int>&comb, vector<vector<int>> &ans , int k  ){
-          if(k < 0 ) return;
+    void solve(int i ,vector<vector<int>>&ans , vector<int>&comb ,int n, int k ){
+        if(k < 0) return;
 
-          if(k == 0 && n == 0){
-              ans.push_back(comb);
-              return;
-          }
+        if(k == 0 && n == 0){
+            ans.push_back(comb);
+            return;
+        }
 
-          for(int j = i ; j < 10 ; j++){
-
+        for(int j = i ; j < 10 ; j++){
             comb.push_back(j);
-            solve(j + 1 , n - j , comb , ans , k - 1);
+            solve(j + 1 , ans , comb , n - j , k - 1);
             comb.pop_back();
-          }
+        } 
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-    
         vector<vector<int>>ans;
         vector<int>comb;
-        solve(1 , n , comb , ans , k);
+        solve(1 , ans , comb , n , k);
         return ans;
     }
 };
