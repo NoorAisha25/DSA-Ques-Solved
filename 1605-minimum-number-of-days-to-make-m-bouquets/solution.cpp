70b5ff1b228f963c673fef1ee20d50baa50d9837
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-15 14:09:15 +0000

    Sync LeetCode submission Runtime - 22 ms (57.11%), Memory - 70.6 MB (48.67%)

--- 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/solution.cpp
@@ -1,41 +1,39 @@
 class Solution {
 public:
-    int solve(vector<int>&arr , int mid , int k){
-        int boq = 0;
-        int curr = 0;
+    
+    int solve(vector<int>&arr , int m , int k){
+        int n = arr.size();
+        int boqcnt = 0;
+        int conscnt = 0;
 
-        for(int i = 0 ; i < arr.size() ; i++){
-              if(arr[i] <= mid){
-                curr++;
-              }
-              else{
-                curr = 0;
-              }
-              if(curr == k){
-                boq++;
-                curr = 0;
-              }
+        for(int i = 0 ; i < n ; i++){
+
+            if(arr[i] <= m) conscnt++;
+            else conscnt = 0;
+            if(conscnt == k){
+                boqcnt++;
+                conscnt = 0;
+            }
         }
-        return boq;
+        return boqcnt;
     }
     int minDays(vector<int>& arr, int m, int k) {
-        int n = arr.size();
-        int l = 1;
-        int h = *max_element(arr.begin() , arr.end());
-        int mini = -1;
+       int n = arr.size();
+       int l = 1;
+       int h = *max_element(arr.begin() , arr.end());
+       int mini = -1;
 
-        while(l <= h){
-            int mid = l + (h  -  l)/2;
+       while(l <= h){
+          int mid = l + (h - l)/2;
 
-            int pos = solve(arr , mid , k);
-            if(pos >= m){
-                mini = mid;
-                h = mid - 1;
-            }
-            else{
-                l = mid + 1 ;
-            }
-        }
-        return mini;
+          int pos = solve(arr , mid , k);
+
+          if(pos >= m){
+            mini = mid;
+            h = mid - 1;
+          }
+          else l = mid + 1;
+       }
+       return mini;
     }
 };
