823396eab0cea4b5563b5b9f06bb30ec1aae9e51
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-15 14:16:10 +0000

    Sync LeetCode submission Runtime - 8 ms (82.53%), Memory - 34.8 MB (99.33%)

--- 1056-capacity-to-ship-packages-within-d-days/solution.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/solution.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    
-    int solve(vector<int>& arr , int m , int d){
-        int cum = 0;
+    int solve(vector<int>&arr , int m){
         int cnt = 1;
+        int sum = 0;
 
         for(int i = 0 ; i < arr.size() ; i++){
-            cum += arr[i];
+            sum += arr[i];
 
-            if(cum > m){
+            if(sum > m){
                 cnt++;
-                cum = arr[i];
+                sum = arr[i];
             }
         }
         return cnt;
     }
     int shipWithinDays(vector<int>& arr, int days) {
        int n = arr.size();
-       int l = *max_element(arr.begin() ,arr.end());
+       int l = *max_element(arr.begin() , arr.end());
        int h = accumulate(arr.begin() , arr.end() , 0);
-       int mini = 1;
+       int mini = -1;
 
        while(l <= h){
-         int m = l + (h - l)/2;
-         int pos = solve(arr , m , days);
+        int m = l + (h -l)/2;
 
-         if(pos <= days){
+        int pos = solve(arr , m);
+
+        if(pos <= days){
             mini = m;
             h = m - 1;
-         }
-         else l = m  + 1;
+        }
+        else l = m + 1;
        }
        return mini;
     }
