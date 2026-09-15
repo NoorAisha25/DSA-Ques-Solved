0a96b019acd99acd41159182ed2d10249761e8dd
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-15 14:38:07 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10.5 MB (73.35%)

--- 0410-split-array-largest-sum/solution.cpp
+++ 0410-split-array-largest-sum/solution.cpp
@@ -1,33 +1,34 @@
 class Solution {
 public:
-    int fun(vector<int>&arr , int m , int k){
+    int func(vector<int>& nums, int m){
         int sum = 0;
         int cnt = 1;
-        for(int i = 0 ; i < arr.size() ; i++){
-            sum += arr[i];
+
+        for(int i = 0 ; i < nums.size() ; i++){
+            sum += nums[i];
 
             if(sum > m){
                 cnt++;
-                sum = arr[i];
+                sum = nums[i];
             }
         }
         return cnt;
     }
-    int solve( vector<int>& arr , int k){
-        int n = arr.size();
-        int l = *max_element(arr.begin() , arr.end());
-        int h = accumulate(arr.begin() , arr.end() , 0);
-        int mini = 0;
+    int solve(vector<int>& nums, int k){
+        int n = nums.size();
+        int l = *max_element(nums.begin() , nums.end());
+        int h = accumulate(nums.begin() , nums.end() , 0);
+        int mini = -1;
 
         while(l <= h){
             int m = l + (h - l)/2;
-            int pos = fun(arr , m , k);
 
+            int pos = func(nums, m);
             if(pos <= k){
                 mini = m;
                 h = m - 1;
             }
-            else l = m  + 1;
+            else l = m + 1;
         }
         return mini;
     }
