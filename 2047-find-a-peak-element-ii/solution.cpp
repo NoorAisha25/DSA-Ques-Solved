a391c677724e05f1ff58614f80b525acad59b60c
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-15 17:01:37 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 50.8 MB (14.79%)

--- 2047-find-a-peak-element-ii/solution.cpp
+++ 2047-find-a-peak-element-ii/solution.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    int fun(vector<vector<int>>& mat , int n , int mid){
-        int maxVal = -1;
-        int rowIndex = -2;
+    int solve(vector<vector<int>>&mat , int m , int n){
+        int maxval = -1;
+        int idx = -2;
 
-        for(int i = 0 ; i<n ; i++){
-            if(mat[i][mid] > maxVal){
-                maxVal = mat[i][mid];
-                rowIndex = i;
+        for(int i = 0 ; i < n ; i++){
+
+            if(mat[i][m] > maxval){
+                maxval = mat[i][m];
+                idx = i;
             }
         }
-        return rowIndex;
+        return idx;
     }
-   
     vector<int> findPeakGrid(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
 
         int l = 0;
-        int h = m-1;
+        int h = m - 1;
 
         while(l <= h){
-           
-           int mid = l + (h-l)/2;
+            int mid = l + (h - l)/2;
 
-           int maxRow = fun( mat, n , mid);
+            int maxrow = solve(mat , mid , n);
 
-           int left = (mid - 1 >= 0) ? mat[maxRow][mid-1] : -1;
-           int right = (mid + 1 < m) ? mat[maxRow][mid+1] : -1;
+            int left = (mid - 1 >= 0) ? mat[maxrow][mid-1] : -1;
+            int right = (mid + 1 < m) ? mat[maxrow][mid+1] : -1;
 
-           if( mat[maxRow][mid] > left && mat[maxRow][mid] > right) {
-               return {maxRow , mid};
-           }
-           else if(mat[maxRow][mid] < left) h = mid - 1;
-           else l = mid + 1;
+            if(mat[maxrow][mid] > left && mat[maxrow][mid]>right){
+                return {maxrow , mid};
+            }
+            else if(mat[maxrow][mid] < left) h = mid - 1;
+            else l = mid + 1;
         }
-        return {-1,-1};
+
+        return {-1 , -1};
     }
 };
