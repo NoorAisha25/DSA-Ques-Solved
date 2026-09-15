01cc5df19ab796b2c25ce67af71f3f7d74f07fae
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-15 14:33:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.2 MB (60.90%)

--- 1646-kth-missing-positive-number/solution.cpp
+++ 1646-kth-missing-positive-number/solution.cpp
@@ -21,13 +21,14 @@ public:
         int n = arr.size();
         int l = 0;
         int h = n - 1;
-
+        
         while(l <= h){
             int m = l + (h - l)/2;
-            int mis = arr[m] - (m + 1);
-            if(mis < k ) l = m  + 1;
+
+            int missnum = arr[m] - (m + 1);
+            if(missnum < k) l = m  + 1;
             else h = m - 1;
         }
-        return (l + k);
+        return l + k;
     }
 };
