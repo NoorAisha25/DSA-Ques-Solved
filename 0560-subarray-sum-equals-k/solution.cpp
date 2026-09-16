9ba6191dc010299d536319d28a3c965bb5cabefa
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-16 08:16:38 +0000

    Sync LeetCode submission Runtime - 29 ms (97.17%), Memory - 45.5 MB (55.08%)

--- 0560-subarray-sum-equals-k/solution.cpp
+++ 0560-subarray-sum-equals-k/solution.cpp
@@ -2,20 +2,23 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
         int n = nums.size();
-        int sum = 0;
+
+        int prefixsum = 0;
         int cnt = 0;
 
         unordered_map<int,int>mp;
-        mp.insert({0,1});
+        mp.insert({0 , 1});
 
         for(int i = 0 ; i < n ; i++){
-            sum += nums[i];
+            prefixsum += nums[i];
 
-            if(mp.find(sum - k ) != mp.end()){
-                cnt += mp[sum-k];
+            if(mp.find(prefixsum-k) != mp.end()){
+                cnt += mp[prefixsum-k];
             }
-            mp[sum]++;
+
+            mp[prefixsum]++;
         }
+
         return cnt;
     }
 };
