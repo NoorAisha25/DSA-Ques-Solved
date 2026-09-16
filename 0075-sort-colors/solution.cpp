c7522fa54e1ab82cbb3cf15d7a63577d40a79459
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-16 08:05:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 11.8 MB (11.31%)

--- 0075-sort-colors/solution.cpp
+++ 0075-sort-colors/solution.cpp
@@ -4,19 +4,19 @@ public:
         int n = nums.size();
         int i = 0;
         int j = 0;
-        int k = n - 1 ;
+        int k = n -1;
+
         while(j <= k){
-            if(nums[j] == 1){
-                j++;
-            }
+
+            if(nums[j] == 1) j++;
             else if(nums[j] == 2){
                 swap(nums[j] , nums[k]);
-                    k--;
+                k--;
             }
-            else if(nums[j] == 0){
+            else{
                 swap(nums[j] , nums[i]);
-                    i++;
-                    j++;
+                i++;
+                j++;
             }
         }
     } 
