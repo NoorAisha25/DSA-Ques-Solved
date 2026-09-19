07b214efb4e7dcfbd754db2b0d6286fb1c3fd88f
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-19 00:59:22 +0000

    Sync LeetCode submission Runtime - 4 ms (4.77%), Memory - 9.1 MB (80.78%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    int solve(int i , long res , string &s , int sign){
-         if(i >= s.size() || !isdigit(s[i])){
-            return (int)(sign * res);
-         }
-           int digit = s[i] - '0';
-
-           if(res > INT_MAX/10 || (res == INT_MAX/10 && digit>7)){
-             return (sign == 1) ? INT_MAX : INT_MIN;
-           }
-
-           res = res * 10 + digit;
-           return solve(i + 1 , res, s , sign);
-    }
     int myAtoi(string s) {
         int n = s.size();
         int sign = 1;
         long res = 0;
-        int i = 0;
+        int i = 0 ;
 
         while(i < n && s[i] == ' ') i++;
 
         if(i < n && (s[i] == '+' || s[i] == '-')){
             sign = (s[i] == '-') ? -1 : 1;
             i++;
-        }       
+        }
 
-        return solve(i , 0 , s , sign);
+        while(i < n && isdigit(s[i])){
+            int d = s[i] - '0';
+
+            if(res > INT_MAX/10 || (res == INT_MAX/10 && d > 7)){
+                return (sign == -1) ? INT_MIN : INT_MAX;
+            }
+
+            res = res * 10 + d;
+            i++;
+        }
+
+        return (int)(sign * res);
     }
 };
