42dc0478e6117485940665c514233ab04cd01a39
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-20 15:24:19 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.1 MB (53.47%)

--- 0008-string-to-integer-atoi/solution.cpp
+++ 0008-string-to-integer-atoi/solution.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int myAtoi(string s) {
         int n = s.size();
-        int sign = 1;
         long res = 0;
-        int i = 0 ;
+        int sign = 1;
+        int i = 0;
 
-        while(i < n && s[i] == ' ') i++;
+        while(i < n && s[i] == ' ')i++;
 
         if(i < n && (s[i] == '+' || s[i] == '-')){
             sign = (s[i] == '-') ? -1 : 1;
