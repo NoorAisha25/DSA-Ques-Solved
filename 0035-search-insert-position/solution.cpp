d2c7aac608ca7a29ade145dac78ec8a7630d7661
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-21 16:11:57 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.7 MB (5.13%)

--- 0035-search-insert-position/solution.cpp
+++ 0035-search-insert-position/solution.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int n= nums.size();
+        int n = nums.size();
         int l = 0;
         int h = n - 1;
-        int ans = n;
+        int res = n;
 
         while(l <= h){
+           int m = l + (h-l)/2;
 
-            int m = l + (h - l)/2;
-
-            if(nums[m] >= target){
-                ans = m;
-                h = m - 1;
-            }
-            else l = m + 1;
+           if(nums[m] >= target){
+             res = m;
+             h = m - 1;
+           }
+           else {
+            l = m + 1;
+           }
         }
-        return ans;
+        return res;
     }
 };
