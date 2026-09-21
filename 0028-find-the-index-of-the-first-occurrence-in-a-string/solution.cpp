e736d6067be031bc793143ad32940e2f180c31a8
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-21 15:48:30 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.2 MB (24.84%)

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/solution.cpp
@@ -3,10 +3,12 @@ public:
     int strStr(string haystack, string needle) {
         int n = haystack.size();
         int m = needle.size();
-        
+
         if(m == 0) return 0;
         if(m > n) return -1;
-        for(int i = 0; i <=n-m ; i++){
+
+        for(int i = 0 ; i <= n - m ; i++){
+
             if(haystack.substr(i , m) == needle){
                 return i;
             }
