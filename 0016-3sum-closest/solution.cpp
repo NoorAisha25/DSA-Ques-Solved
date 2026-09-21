ce315a3c3993eeb8931855f3fb081b2cc5c55c73
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-21 14:16:06 +0000

    Sync LeetCode submission Runtime - 14 ms (35.51%), Memory - 14 MB (85.63%)

--- 0016-3sum-closest/solution.cpp
+++ 0016-3sum-closest/solution.cpp
@@ -3,21 +3,25 @@ public:
     int threeSumClosest(vector<int>& nums, int target) {
         int n = nums.size(); 
         sort(nums.begin() , nums.end());
-        int closest = nums[0]  +nums[1] + nums[2];
+        int closest = nums[0] + nums[1] + nums[2];
 
         for(int i = 0 ; i < n ; i++){
-            int l = i + 1;
-            int h = n - 1;
-            while ( l < h){
-                int sum = nums[i] + nums[l] + nums[h];
-                if(abs(sum - target) < abs(closest - target)){
-                    closest = sum;
-                }
+          int l = i + 1;
+          int h = n - 1;
 
-                if(sum > target) h--;
-                else if(sum < target) l++;
-                else return sum;
-            }
+          while(l < h){
+              int sum = nums[i] + nums[l] + nums[h];
+
+              if(abs(sum - target) < abs(closest - target)){
+                closest = sum;
+              }
+
+              if(sum < target) l++;
+              else if( sum > target) h--;
+              else{
+                 return sum;
+              }
+          }
         }
         return closest;
     }
