c630764689ab8b11f986afff33508255fd55ad61
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-21 16:08:10 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 15.2 MB (37.58%)

--- 0033-search-in-rotated-sorted-array/solution.cpp
+++ 0033-search-in-rotated-sorted-array/solution.cpp
@@ -6,7 +6,7 @@ public:
         int h = n - 1;
 
         while(l <= h){
-            int m = l + (h - l)/2;
+            int m = l + ( h - l)/2;
 
             if(nums[m] == target) return m;
 
@@ -14,8 +14,7 @@ public:
                 if(nums[l] <= target && target <= nums[m]) h = m - 1;
                 else l = m + 1;
             }
-
-            else {
+            else{
                 if(nums[m] <= target && target <= nums[h]) l = m + 1;
                 else h = m - 1;
             }
