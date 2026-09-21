8a433ad78802f5d972cf0e26ec063ec300537b9d
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-21 14:31:24 +0000

    Sync LeetCode submission Runtime - 19 ms (81.20%), Memory - 17.5 MB (58.49%)

--- 0018-4sum/solution.cpp
+++ 0018-4sum/solution.cpp
@@ -1,40 +1,38 @@
 class Solution {
 public:
-    
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
        int n = nums.size();
        if(n < 4) return {};
 
-       vector<vector<int>>res;
        sort(nums.begin() , nums.end());
+       vector<vector<int>>res;
 
-       for(int i = 0 ; i < n - 3 ; i++){
-          if(i > 0 && nums[i] == nums[i-1]) continue;
+       for(int i = 0 ; i < n-3 ; i++){
+           if(i > 0 && nums[i] == nums[i-1])continue;
 
-          int n1 = nums[i];
-          for(int l = i + 1 ; l < n - 2 ; l++){
-               if(l > i+1 && nums[l] == nums[l-1]) continue;
+           int n1 = nums[i];
+           for(int j = i + 1 ; j < n - 2 ; j++){
+               if(j > i+1 && nums[j] == nums[j-1]) continue;
 
-               int n2 = nums[l];
-               long long newt = (long long) target - n1 - n2;
-                int j = l+1;
-                int k = n -1;
-                int sum = 0;
+               int n2 = nums[j];
+               long long newtarget = (long long)target - n1 - n2;
+               int l = j + 1;
+               int h = n - 1;
+               int sum = 0 ;
+               while(l < h ){
+                  sum = nums[l] + nums[h];
+                  if(sum < newtarget) l++;
+                  else if(sum > newtarget) h--;
+                  else{
+                     while(l < h && nums[l] == nums[l+1]) l++;
+                     while(l < h && nums[h] == nums[h-1]) h--;
 
-                while(j < k){
-                    sum = nums[j] + nums[k];
-                    if(sum > newt) k--;
-                    else if(sum < newt) j++;
-                    else{
-                        while(j < k && nums[j] == nums[j+1]) j++;
-                        while(j < k && nums[k] == nums[k-1]) k--;
-
-                        res.push_back({nums[i] , nums[l], nums[j] , nums[k]});
-                        j++;
-                        k--;
-                    }
-                }
-                }
+                     res.push_back({nums[i] , nums[j] , nums[l] , nums[h]});
+                     l++;
+                     h--;
+                  }
+               }
+           }
        }
        return res;
     }
