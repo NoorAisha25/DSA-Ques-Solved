f1d4cdb6f694540a28176af0cc2abcf4ad6cbcfc
NoorAisha25 <noor079btit23@igdtuw.ac.in>
2026-09-22 15:11:24 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 36.4 MB (18.19%)

---
+++ 4418-number-of-intersecting-interval-pairs-i/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countIntersectingIntervals(vector<vector<int>>& intervals) {
+        
+        long long cnt = 0;
+        
+        for(int i = 0 ; i < intervals.size() ; i++){
+            for(int j = i + 1 ; j < intervals.size(); j++){
+                if(max(intervals[i][0] , intervals[j][0]) <= min(intervals[i][1] , intervals[j][1])){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+};
